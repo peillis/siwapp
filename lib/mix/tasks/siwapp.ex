12b0ce97f57a097f4326181dcedf77caeb4d5017
Sergio Olivares <49884623+olirods@users.noreply.github.com>
2021-12-14 19:53:15 +0100

    Sergio/register script (#8)
    * Script for registering an user done
    
    * More secure signing salt for API token
    
    * Script for registering an user done
    
    * More secure signing salt for API token
    
    * mix format
    
    * Solve problems with mix and dialyzer
    
    * .dialyzer_ignore.exs shrinked to one line

---
+++ lib/mix/tasks/siwapp.ex
@@ -0,0 +1,27 @@
+defmodule Mix.Tasks.Siwapp do
+  use Mix.Task
+
+  @shortdoc "Prints Siwapp scripts help information"
+
+  @impl true
+  def run(args) do
+    {_opts, args} = OptionParser.parse!(args, strict: [])
+
+    case args do
+      [] -> general()
+      _ -> Mix.raise("Invalid arguments, expected: mix siwapp")
+    end
+  end
+
+  defp general() do
+    Application.ensure_all_started(:ecto)
+    Mix.shell().info("Siwapp")
+
+    Mix.shell().info(
+      "An open source web application meant to help manage and create invoices in a simple, straightforward way."
+    )
+
+    Mix.shell().info("\nAvailable tasks:\n")
+    Mix.Tasks.Help.run(["--search", "siwapp."])
+  end
+end
