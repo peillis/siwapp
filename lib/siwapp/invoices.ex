29549f2ea159aa0606aaf2212834c836cfa6dcd5
Rodrigo Molina <rodrigomolina@doofinder.com>
2021-12-14 10:59:44 +0100

    New improvements on the JSON api. And some changes on the Siwapp.Invoices module for the context

--- lib/siwapp/invoices.ex
+++ lib/siwapp/invoices.ex
@@ -5,23 +5,33 @@ defmodule Siwapp.Invoices do
 
   import Ecto.Query, warn: false
   alias Siwapp.Repo
-
   alias Siwapp.Schema.Invoices
 
   @doc """
-  Gets a list of invoices by updated date.
+  Gets a list of invoices by updated date
   """
   def list_invoices() do
-    Repo.all(
-      from(
-        i in Invoices,
-        order_by: [desc: i.updated_at]
-      )
-    )
+    # query = Query.invoices()
+    Repo.all(Invoices)
   end
 
   @doc """
-  Creates an invoice.
+  Gets a list on the invoices that macht with the paramas
+  """
+  def list_invoices_by(_key, _value) do
+    # query = Query.invoices_by(key, value)
+    Repo.all(Invoices)
+  end
+
+  @doc """
+  Gets all the information about the specified invoice
+  """
+  def show_invoice(id) do
+    Repo.get(Invoices, id)
+  end
+
+  @doc """
+  Creates an invoice
   """
   def create_invoice(attrs \\ %{}) do
     %Invoices{}
@@ -30,7 +40,7 @@ defmodule Siwapp.Invoices do
   end
 
   @doc """
-  Updates an invoice.
+  Update an invoice
   """
   def update_invoice(%Invoices{} = invoice, attrs) do
     invoice
@@ -39,9 +49,13 @@ defmodule Siwapp.Invoices do
   end
 
   @doc """
-  Deletes an invoice.
+  Delete an invoice
   """
   def delete_invoice(%Invoices{} = invoice) do
     Repo.delete(invoice)
   end
+  @doc """
+  Gets an invoice by id
+  """
+  def get_invoice!(id), do: Repo.get!(Invoices, id)
 end
