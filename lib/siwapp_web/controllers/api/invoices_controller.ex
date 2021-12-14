899557bcf40749dde9c2f32d7a568f08002a29ec
RodrigoMolinaMadrid <93586187+RodrigoMolinaMadrid@users.noreply.github.com>
2021-12-14 13:05:29 +0100

    Rodri/invoices api (#7)
    * New improvements on the JSON api. And some changes on the Siwapp.Invoices module for the context
    
    * Some reviews in the Siwapp.Invoices module
    
    * New changes
    
    Co-authored-by: Rodrigo Molina <rodrigomolina@doofinder.com>

--- lib/siwapp_web/controllers/api/invoices_controller.ex
+++ lib/siwapp_web/controllers/api/invoices_controller.ex
@@ -7,28 +7,42 @@ defmodule SiwappWeb.InvoicesController do
     json(conn, invoices)
   end
 
-  def searching(conn, params) do
-    json(conn, params)
+  def searching(conn, %{"map" => map}) do
+    {key, value} =
+      map
+      |> String.split()
+      |> List.to_tuple()
+
+    search = Invoices.list_invoices_by(key, value)
+    json(conn, search)
   end
 
-  def show(conn, params) do
-    id = Map.get(params, "id")
-    json(conn, id)
-  end
-
-  def create(conn, invoice) do
+  def show(conn, %{"id" => id}) do
+    invoice = Invoices.get_invoice!(id)
     json(conn, invoice)
   end
 
-  def update(conn, updates) do
-    json(conn, updates)
+  def create(conn, %{"invoice" => invoice_params}) do
+    case Invoices.create_invoice(invoice_params) do
+      {:ok, _} -> json(conn, "The invoice was successfully created")
+      {:error, changeset} -> json(conn, changeset)
+    end
+  end
+
+  def update(conn, %{"id" => invoice_params}) do
+    case Invoices.update_invoice(conn.assigns.invoice, invoice_params) do
+      {:ok, _} -> json(conn, "The invoice was successfully updated")
+      {:error, changeset} -> json(conn, changeset)
+    end
   end
 
   def send_email(conn, params) do
     json(conn, params)
   end
 
-  def delete(conn, params) do
-    json(conn, params)
+  def delete(conn, %{"id" => id}) do
+    invoice = Invoices.get_invoice!(id)
+    {:ok, response} = Invoices.delete_invoice(invoice)
+    json(conn, response)
   end
 end
