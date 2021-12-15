2fe373da45ae827e5bda777ca17fda56b3830088
Sergio Olivares <sergio.olivares@doofinder.com>
2021-12-15 10:51:12 +0100

    Plugged the changeset

--- lib/siwapp_web/controllers/series_controller.ex
+++ lib/siwapp_web/controllers/series_controller.ex
@@ -1,6 +1,14 @@
 defmodule SiwappWeb.SeriesController do
   use SiwappWeb, :controller
 
+  plug :assign_email_and_password_changesets
 
+  defp assign_email_and_password_changesets(conn, _opts) do
+    user = conn.assigns.current_user
+
+    conn
+    |> assign(:email_changeset, Accounts.change_user_email(user))
+    |> assign(:password_changeset, Accounts.change_user_password(user))
+  end
 
 end
