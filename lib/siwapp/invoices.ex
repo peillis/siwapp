cbd40855052246b15f997dc62b9d4f59c8cdc147
Rodrigo Molina <rodrigomolina@doofinder.com>
2021-12-15 12:15:10 +0100

    Changing the path of the apitoken file

--- lib/siwapp/invoices.ex
+++ lib/siwapp/invoices.ex
@@ -6,11 +6,12 @@ defmodule Siwapp.Invoices do
   import Ecto.Query, warn: false
   alias Siwapp.Repo
   alias Siwapp.Schema.Invoices
+  alias Siwapp.Schema.Customers
 
   @doc """
   Gets a list of invoices by updated date
   """
-  def list_invoices() do
+  def list() do
     # query = Query.invoices()
     Repo.all(Invoices)
   end
@@ -18,7 +19,7 @@ defmodule Siwapp.Invoices do
   @doc """
   Gets a list on the invoices that macht with the paramas
   """
-  def list_invoices_by(_key, _value) do
+  def list_by(_key, _value) do
     # query = Query.invoices_by(key, value)
     Repo.all(Invoices)
   end
@@ -26,7 +27,7 @@ defmodule Siwapp.Invoices do
   @doc """
   Creates an invoice
   """
-  def create_invoice(attrs \\ %{}) do
+  def create(attrs \\ %{}) do
     %Invoices{}
     |> Invoices.changeset(attrs)
     |> Repo.insert()
@@ -35,7 +36,7 @@ defmodule Siwapp.Invoices do
   @doc """
   Update an invoice
   """
-  def update_invoice(%Invoices{} = invoice, attrs) do
+  def update(%Invoices{} = invoice, attrs) do
     invoice
     |> Invoices.changeset(attrs)
     |> Repo.update()
@@ -44,11 +45,50 @@ defmodule Siwapp.Invoices do
   @doc """
   Delete an invoice
   """
-  def delete_invoice(%Invoices{} = invoice) do
+  def delete(%Invoices{} = invoice) do
     Repo.delete(invoice)
   end
+
   @doc """
   Gets an invoice by id
   """
-  def get_invoice!(id), do: Repo.get!(Invoices, id)
+  def get!(id), do: Repo.get!(Invoices, id)
+
+  @doc """
+  Get a single invoice by the params
+  """
+  def get_by!(key,value) do
+    Repo.get_by!(Invoices, %{key => value})
+  end
+
+  @doc """
+  Create a new customer
+  """
+  def create_customer(attrs \\ %{}) do
+    %Customers{}
+    |> Customers.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Update a customer
+  """
+  def update_customer(%Customers{} = customer, attrs) do
+    customer
+    |> Customers.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @doc """
+  Delete a customer
+  """
+  def delete_customer(%Customers{} = customer) do
+    Repo.delete(customer)
+  end
+
+  @doc """
+  Gets a customer by id
+  """
+  def get_customer!(id), do: Repo.get!(Customers, id)
+
 end
