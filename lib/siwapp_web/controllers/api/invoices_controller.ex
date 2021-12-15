6ced1bed8d0ba1f0b495e73b04ab636b8b13e78e
Rodrigo Molina <rodrigomolina@doofinder.com>
2021-12-15 09:34:22 +0100

    Requested changes of the PR

--- lib/siwapp_web/controllers/api/invoices_controller.ex
+++ lib/siwapp_web/controllers/api/invoices_controller.ex
@@ -1,9 +1,9 @@
 defmodule SiwappWeb.InvoicesController do
   use SiwappWeb, :controller
-  alias Siwapp.InvoicesContext
+  alias Siwapp.Invoices
 
   def list(conn, _params) do
-    invoices = InvoicesContext.list_invoices()
+    invoices = Invoices.list()
     json(conn, invoices)
   end
 
@@ -13,24 +13,24 @@ defmodule SiwappWeb.InvoicesController do
       |> String.split()
       |> List.to_tuple()
 
-    search = InvoicesContext.list_invoices_by(key, value)
+    search = Invoices.list_by(key, value)
     json(conn, search)
   end
 
   def show(conn, %{"id" => id}) do
-    invoice = InvoicesContext.get_invoice!(id)
+    invoice = Invoices.get!(id)
     json(conn, invoice)
   end
 
   def create(conn, %{"invoice" => invoice_params}) do
-    case InvoicesContext.create_invoice(invoice_params) do
+    case Invoices.create(invoice_params) do
       {:ok, _} -> json(conn, "The invoice was successfully created")
       {:error, changeset} -> json(conn, changeset)
     end
   end
 
   def update(conn, %{"id" => invoice_params}) do
-    case InvoicesContext.update_invoice(conn.assigns.invoice, invoice_params) do
+    case Invoices.update(conn.assigns.invoice, invoice_params) do
       {:ok, _} -> json(conn, "The invoice was successfully updated")
       {:error, changeset} -> json(conn, changeset)
     end
@@ -41,8 +41,8 @@ defmodule SiwappWeb.InvoicesController do
   end
 
   def delete(conn, %{"id" => id}) do
-    invoice = InvoicesContext.get_invoice!(id)
-    {:ok, response} = InvoicesContext.delete_invoice(invoice)
+    invoice = Invoices.get!(id)
+    {:ok, response} = Invoices.delete(invoice)
     json(conn, response)
   end
 end
