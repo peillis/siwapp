9fd5854625c2c7acba4119c2e99fc103a762853d
nurietzche <nuria.gallego@doofinder.com>
2021-12-17 14:43:27 +0100

    Fixed

--- lib/siwapp/schema/meta_attributes.ex
+++ lib/siwapp/schema/meta_attributes.ex
@@ -11,9 +11,6 @@ defmodule Siwapp.Schema.MetaAttributes do
   def changeset(meta_attribute, attrs) do
     meta_attribute
     |> cast(attrs, [:key, :value])
-<<<<<<< HEAD
     |> validate_required([:key, :value])
-=======
->>>>>>> 49d14a439e4063eef25fb277bcca392b57831cae
   end
 end
