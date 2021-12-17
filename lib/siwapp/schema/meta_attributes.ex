4ab9faedc1a7f16164e1fa38a464ff16298c8e68
nurietzche <nuria.gallego@doofinder.com>
2021-12-17 14:35:25 +0100

    Creation of MetaAttributes Schema

--- lib/siwapp/schema/meta_attributes.ex
+++ lib/siwapp/schema/meta_attributes.ex
@@ -11,6 +11,9 @@ defmodule Siwapp.Schema.MetaAttributes do
   def changeset(meta_attribute, attrs) do
     meta_attribute
     |> cast(attrs, [:key, :value])
+<<<<<<< HEAD
     |> validate_required([:key, :value])
+=======
+>>>>>>> 49d14a439e4063eef25fb277bcca392b57831cae
   end
 end
