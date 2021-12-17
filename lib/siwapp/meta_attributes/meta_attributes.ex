43dcd9651b6367c334bcde534e63c33e327f30d1
nurietzche <nuria.gallego@doofinder.com>
2021-12-17 14:43:27 +0100

    All conflicts of rebase fixed

---
+++ lib/siwapp/meta_attributes/meta_attributes.ex
@@ -0,0 +1,16 @@
+defmodule Siwapp.Schema.MetaAttributes do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key false
+  embedded_schema do
+    field :key, :string
+    field :value, :string
+  end
+
+  def changeset(meta_attribute, attrs) do
+    meta_attribute
+    |> cast(attrs, [:key, :value])
+    |> validate_required([:key, :value])
+  end
+end
