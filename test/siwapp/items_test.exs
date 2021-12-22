a545f16c95c881d617d8905304fd84af0293fb82
ale1311sv <alejandro.sarabia@doofinder.com>
2021-12-22 18:35:10 +0100

    Fixed the tests. Now, it works correctly.

--- test/siwapp/items_test.exs
+++ test/siwapp/items_test.exs
@@ -5,22 +5,26 @@ defmodule Siwapp.ItemsTest do
   alias Siwapp.Invoices.Item
 
   describe "base_amount(item) Result of the multiply between the quantity and the unitary cost of an item" do
-    test "return the base amount if quantity and unitary cost are well defined"
-    assert Item.base_amount(%Item{quantity: 2, unitary_cost: 10}) == 20
+    test "return the base amount if quantity and unitary cost are well defined" do
+    item = %Item{quantity: 2, unitary_cost: 10}
+    assert Item.base_amount(item) == 20
+    end
   end
 
   describe "discount_amount(item) Total discount amount given a discount" do
-    test "return the discount amount if discount is well defined"
+    test "return the discount amount if discount is well defined" do
     assert Item.discount_amount(%Item{quantity: 2, unitary_cost: 10, discount: 50}) == 10
+    end
   end
 
   describe "net_amount(item) Substraction between base amount and discount amount" do
-    test "return the net amount if either base_amount/1 and discount_amount/1 does not return errors"
+    test "return the net amount if either base_amount/1 and discount_amount/1 does not return errors" do
     assert Item.net_amount(%Item{quantity: 2, unitary_cost: 10, discount: 50}) == 10
+    end
   end
 
   describe "taxes_amount(item) Takes the taxes value for those taxes associated to the correspondent item and calculate the taxes amount" do
-    test "return a list if a tax or a group of tax exists"
+    test "return a list if a tax or a group of tax exists" do
 
     assert Item.taxes_amount(%Item{
              taxes: [%Tax{value: 20}],
@@ -28,10 +32,11 @@ defmodule Siwapp.ItemsTest do
              unitary_cost: 10,
              discount: 50
            }) == [2]
+    end
   end
 
   describe "gross_amount(item) Sume betwwen net_amount and taxes_amount" do
-    test "return the gross amount if either net_amount/1 and taxes_amount/1 does not return errors"
+    test "return the gross amount if either net_amount/1 and taxes_amount/1 does not return errors" do
 
     assert Item.gross_amount(%Item{
              taxes: [%Tax{value: 20}],
@@ -39,5 +44,6 @@ defmodule Siwapp.ItemsTest do
              unitary_cost: 10,
              discount: 50
            }) == 12
+    end
   end
 end
