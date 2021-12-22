57e398dd15d460c314fcca933a2846f0acc0ea79
peillis <enrique@doofinder.com>
2021-12-22 18:28:01 +0100

    adding attrs

--- lib/siwapp_web/live/meta_attributes_component.ex
+++ lib/siwapp_web/live/meta_attributes_component.ex
@@ -10,16 +10,32 @@ defmodule SiwappWeb.MetaAttributesComponent do
         <a phx-click="remove" phx-value-key={k} phx-target={@myself}>Remove</a>
         <br/>
       <% end %>
-      <input type="text" name="meta[keys][]" />
-      <input type="text" name="meta[values][]" />
+      <input type="text" name="meta[keys][]" phx-blur="changing-key" phx-target={@myself} />
+      <input type="text" name="meta[values][]" phx-blur="changing-value" phx-target={@myself} />
+      <a phx-click="add" phx-target={@myself}>Add</a>
       <br/>
     </fieldset>
     """
   end
 
   def handle_event("remove", %{"key" => key}, socket) do
-    new_meta_attributes = Map.delete(socket.assigns.meta_attributes, key)
-    send_update(__MODULE__, id: "meta_attributes", meta_attributes: new_meta_attributes)
+    new = Map.delete(socket.assigns.meta_attributes, key)
+    send_update(__MODULE__, id: "meta_attributes", meta_attributes: new)
+
+    {:noreply, socket}
+  end
+
+  def handle_event("changing-key", %{"value" => value}, socket) do
+    {:noreply, assign(socket, :new_key, value)}
+  end
+
+  def handle_event("changing-value", %{"value" => value}, socket) do
+    {:noreply, assign(socket, :new_value, value)}
+  end
+
+  def handle_event("add", _params, socket) do
+    new = Map.put(socket.assigns.meta_attributes, socket.assigns.new_key, socket.assigns.new_value)
+    send_update(__MODULE__, id: "meta_attributes", meta_attributes: new)
 
     {:noreply, socket}
   end
