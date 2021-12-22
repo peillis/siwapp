2ba0ab8d45921f59b8c590dc3e67881fea421d56
Sergio Olivares <sergio.olivares@doofinder.com>
2021-12-22 18:59:00 +0100

    mix format

--- mix.exs
+++ mix.exs
@@ -54,7 +54,8 @@ defmodule Siwapp.MixProject do
       {:swoosh, "~> 1.3"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
-      {:contex, "~> 0.4.0"}    ]
+      {:contex, "~> 0.4.0"}
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
