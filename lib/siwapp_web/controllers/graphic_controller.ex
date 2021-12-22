30d7cd35089be82164f47cd60a2bb9ba635f5c90
Sergio Olivares <sergio.olivares@doofinder.com>
2021-12-22 15:48:17 +0100

    Line plot started

--- lib/siwapp_web/controllers/graphic_controller.ex
+++ lib/siwapp_web/controllers/graphic_controller.ex
@@ -1,29 +1,35 @@
 defmodule SiwappWeb.GraphicController do
 
-  alias Contex.{BarChart, Plot, Dataset}
+  alias Contex.{LinePlot, Plot, Dataset, TimeScale}
   alias Siwapp.Invoices
 
   def plot() do
     options = [
-      mapping: %{category_col: :issue_date, value_cols: [:gross_amount]},
       data_labels: false,
-      colour_palette: ["bbd5ec"],
       default_style: false,
+      smoothed: false
     ]
 
-    invoices_data =
       Invoices.list()
       |> Enum.map(&(Map.take(&1, [:issue_date, :gross_amount])))
+      |> Enum.map(fn %{issue_date: x} = data -> %{data | issue_date: elem(NaiveDateTime.new(x, ~T[00:00:00]), 1)} end)
+      |> IO.inspect
+      |> accumulate_amounts()
+      |> Dataset.new()
+      |> Plot.new(LinePlot, 500, 200, options)
+      |> Plot.to_svg()
+  end
 
-    Date.utc_today
-    |> Date.add(-30)
-    |> Date.range(Date.utc_today)
-    |> Enum.map(&( %{issue_date: &1, gross_amount: 0}))
-    |> Enum.map(fn value -> Map.merge(value, Enum.find(invoices_data, %{}, &(&1[:issue_date] == value[:issue_date]))) end)
-    |> Enum.map(fn %{issue_date: x} = data -> %{data | issue_date: Calendar.strftime(x, "%d %b")} end)
-    |> Dataset.new()
-    |> Plot.new(BarChart, 500, 200, options)
-    |> Plot.to_svg()
+  defp accumulate_amounts(data) do
+    data
+    |> Enum.group_by(&(&1[:issue_date]))
+    |> Map.values()
+    |> Enum.map(&({hd(&1).issue_date, sum_amounts(&1)}))
+    |> IO.inspect
+  end
+
+  defp sum_amounts(list) do
+    Enum.sum(Enum.map(list, &(&1.gross_amount)))
   end
 
 end
