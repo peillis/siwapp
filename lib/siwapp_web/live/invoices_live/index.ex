3da3d4bd249f893367430335d8e70c17556eb1b5
Pabloj20 <pablo.rubio@doofinder.com>
2021-12-23 11:13:49 +0100

    all checked event added

--- lib/siwapp_web/live/invoices_live/index.ex
+++ lib/siwapp_web/live/invoices_live/index.ex
@@ -9,6 +9,26 @@ defmodule SiwappWeb.InvoicesLive.Index do
      |> assign(:checked, MapSet.new())}
   end
 
+  def handle_event("show_buttons?", %{"id" => "0", "value" => "on"}, socket) do
+    update_checked = add_all_ids(socket.assigns.checked, socket.assigns.invoices)
+
+    {:noreply,
+     assign(
+       socket,
+       checked: update_checked
+     )}
+  end
+
+  def handle_event("show_buttons?", %{"id" => "0"}, socket) do
+    update_checked = del_all_ids(socket.assigns.checked, socket.assigns.invoices)
+
+    {:noreply,
+     assign(
+       socket,
+       checked: update_checked
+     )}
+  end
+
   def handle_event("show_buttons?", %{"id" => id, "value" => "on"}, socket) do
     update_checked = MapSet.put(socket.assigns.checked, id)
 
@@ -22,10 +42,40 @@ defmodule SiwappWeb.InvoicesLive.Index do
   def handle_event("show_buttons?", %{"id" => id}, socket) do
     update_checked = MapSet.delete(socket.assigns.checked, id)
 
-    {:noreply,
-     assign(
-       socket,
-       checked: update_checked
-     )}
+    if MapSet.size(update_checked) == 1 do
+      update_checked = MapSet.delete(update_checked, "0")
+
+      {:noreply,
+       assign(
+         socket,
+         checked: update_checked
+       )}
+    else
+      {:noreply,
+       assign(
+         socket,
+         checked: update_checked
+       )}
+    end
+  end
+
+  defp add_all_ids(checked, invoices) do
+    add_zero =
+      checked
+      |> MapSet.put("0")
+
+    invoices
+    |> Enum.reduce(add_zero, fn invoice, acc -> MapSet.put(acc, Integer.to_string(invoice.id)) end)
+  end
+
+  defp del_all_ids(checked, invoices) do
+    del_zero =
+      checked
+      |> MapSet.delete("0")
+
+    invoices
+    |> Enum.reduce(del_zero, fn invoice, acc ->
+      MapSet.delete(acc, Integer.to_string(invoice.id))
+    end)
   end
 end
