671d63d62a021350a79caadba58a51907cff2f1f
nurietzche <nuria.gallego@doofinder.com>
2021-12-23 11:37:52 +0100

    Improvement of nav-bar adequating "New" buttons

--- lib/siwapp_web/views/layout_view.ex
+++ lib/siwapp_web/views/layout_view.ex
@@ -4,4 +4,14 @@ defmodule SiwappWeb.LayoutView do
   # Phoenix LiveDashboard is available only in development by default,
   # so we instruct Elixir to not warn if the dashboard route is missing.
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
+
+  def new_button(live_module) do
+    live_module = to_string(live_module)
+    cond do
+      live_module =~ "Customers" -> "New Customer"
+      live_module =~ "Invoices" -> "New Invoice"
+      live_module =~ "Series" -> "New Series"
+    end
+  end
+
 end
