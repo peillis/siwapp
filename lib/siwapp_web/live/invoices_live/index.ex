78b1914ef83f1198487d5264d44ccead0b6cb5d4
Pabloj20 <pablo.rubio@doofinder.com>
2021-12-23 13:28:22 +0100

    Use only one handle_event calling to update_checked private function

--- lib/siwapp_web/live/invoices_live/index.ex
+++ lib/siwapp_web/live/invoices_live/index.ex
@@ -9,60 +9,38 @@ defmodule SiwappWeb.InvoicesLive.Index do
      |> assign(:checked, MapSet.new())}
   end
 
-  def handle_event("click_checkbox", %{"id" => "0", "value" => "on"}, socket) do
-    update_checked = add_all_ids(socket.assigns.invoices)
+  def handle_event("click_checkbox", data, socket) do
+    checked = update_checked(data, socket)
 
     {:noreply,
      assign(
        socket,
-       checked: update_checked
+       checked: checked
      )}
   end
 
-  def handle_event("click_checkbox", %{"id" => "0"}, socket) do
-    {:noreply,
-     assign(
-       socket,
-       checked: MapSet.new()
-     )}
-  end
-
-  def handle_event("click_checkbox", %{"id" => id, "value" => "on"}, socket) do
-    update_checked = MapSet.put(socket.assigns.checked, id)
-
-    {:noreply,
-     assign(
-       socket,
-       checked: update_checked
-     )}
-  end
-
-  def handle_event("click_checkbox", %{"id" => id}, socket) do
-    update_checked = MapSet.delete(socket.assigns.checked, id)
-
-    if MapSet.size(update_checked) == 1 and MapSet.member?(update_checked, "0") do
-      {:noreply,
-       assign(
-         socket,
-         checked: MapSet.new()
-       )}
-    else
-      {:noreply,
-       assign(
-         socket,
-         checked: update_checked
-       )}
-    end
-  end
-
-  defp add_all_ids(invoices) do
+  defp update_checked(%{"id" => "0", "value" => "on"}, socket) do
     add_zero =
       MapSet.new()
       |> MapSet.put("0")
 
-    invoices
+    socket.assigns.invoices
     |> Enum.reduce(add_zero, fn invoice, mapset ->
       MapSet.put(mapset, Integer.to_string(invoice.id))
     end)
+  end
+
+  defp update_checked(%{"id" => "0"}, _) do
+    MapSet.new()
+  end
+
+  defp update_checked(%{"id" => id, "value" => "on"}, socket) do
+    MapSet.put(socket.assigns.checked, id)
+  end
+
+  defp update_checked(%{"id" => id}, socket) do
+    socket.assigns.checked
+    |> MapSet.delete(id)
+    |> MapSet.delete("0")
   end
 end
