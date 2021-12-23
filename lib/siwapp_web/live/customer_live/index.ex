5419cf2af55cf1404f02d43ac47dc227f5fadcbe
nurietzche <nuria.gallego@doofinder.com>
2021-12-23 11:39:38 +0100

    Creation of customers index

--- lib/siwapp_web/live/customer_live/index.ex
+++ lib/siwapp_web/live/customer_live/index.ex
@@ -1,1 +1,22 @@
+defmodule SiwappWeb.CustomersLive.Index do
+  use SiwappWeb, :live_view
+
+  alias Siwapp.Customers
+
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, customers: Customers.list(), page_title: "Customers")}
+  end
+
+  def handle_event("see-invoices", %{ "id" => id }, socket) do
+    {:noreply, push_redirect(socket, to: Routes.customers_show_invoices_path(socket, :show, id) )}
+  end
+  def handle_event("edit", %{ "id" => id } = params, socket) do
+    {:noreply, push_redirect(socket, to: Routes.customers_edit_path(socket, :edit, id))}
+  end
+end
+
+
+
+
+
 
