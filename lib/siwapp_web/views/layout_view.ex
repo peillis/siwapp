95a585c53f8f7eac06484efca4db699a965f87b9
nurietzche <nuria.gallego@doofinder.com>
2021-12-23 12:29:33 +0100

    Refactor of new_button

--- lib/siwapp_web/views/layout_view.ex
+++ lib/siwapp_web/views/layout_view.ex
@@ -5,13 +5,19 @@ defmodule SiwappWeb.LayoutView do
   # so we instruct Elixir to not warn if the dashboard route is missing.
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
 
-  def new_button(live_module) do
-    live_module = to_string(live_module)
+  def new_button(assigns) do
+    if assigns.conn.private.phoenix_template == "index.html" do
+      "New Invoice"
+    else
+      live_module = to_string(assigns.live_module)
 
     cond do
       live_module =~ "Customers" -> "New Customer"
       live_module =~ "Invoices" -> "New Invoice"
       live_module =~ "Series" -> "New Series"
+      # explorable in future if more views are needed
+      true -> ""
     end
+  end
   end
 end
