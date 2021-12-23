8cb826d81edd7bb17f31f2ec280d30c4a4695f2f
nurietzche <nuria.gallego@doofinder.com>
2021-12-23 12:30:11 +0100

    Last refactor of new_button

--- lib/siwapp_web/views/layout_view.ex
+++ lib/siwapp_web/views/layout_view.ex
@@ -6,18 +6,14 @@ defmodule SiwappWeb.LayoutView do
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
 
   def new_button(assigns) do
-    if assigns.conn.private.phoenix_template == "index.html" do
-      "New Invoice"
-    else
-      live_module = to_string(assigns.live_module)
+    path = assigns.conn.request_path
 
     cond do
-      live_module =~ "Customers" -> "New Customer"
-      live_module =~ "Invoices" -> "New Invoice"
-      live_module =~ "Series" -> "New Series"
-      # explorable in future if more views are needed
-      true -> ""
+      path =~ "taxes" -> "New tax"
+      path =~ "customers" -> "New Customer"
+      path =~ "series" -> "New Series"
+      path =~ "templates" -> "New Template"
+      true -> "New Invoice"
     end
-  end
   end
 end
