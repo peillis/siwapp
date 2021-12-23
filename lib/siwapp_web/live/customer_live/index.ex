8199718cbf4f117a68e272ed82b973663303488c
nurietzche <nuria.gallego@doofinder.com>
2021-12-23 16:53:16 +0100

    Customer index working LACK OF: Precise data for due and total and navigation bar functioning Commented in html you can see old managing of edit_customer redirection using events. In this version, I had to insert <a href ... ></a> to each cell because other ways of using links seem uncompatible with bulma table.

--- lib/siwapp_web/live/customer_live/index.ex
+++ lib/siwapp_web/live/customer_live/index.ex
@@ -1,1 +1,15 @@
+defmodule SiwappWeb.CustomerLive.Index do
+  use SiwappWeb, :live_view
 
+  alias Siwapp.Customers
+
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, customers: Customers.list(), page_title: "Customers")}
+  end
+
+  def path_edit_customer(id), do: "/customers/" <> to_string(id) <> "/edit"
+
+  def handle_event("edit", %{"id" => id}, socket) do
+    {:noreply, push_redirect(socket, to: Routes.customer_edit_path(socket, :edit, id))}
+  end
+end
