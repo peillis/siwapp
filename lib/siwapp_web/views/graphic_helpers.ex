8922d193c5b273986965992ac6ca613528b4e116
Sergio Olivares <49884623+olirods@users.noreply.github.com>
2021-12-27 12:25:06 +0100

    Sergio/graphic fix #59 (#84)
    * Initial sketched version
    
    * CSS and seeds
    
    * Graphic working. Not perfect yet.
    
    * Line plot started
    
    * Line plot working. Typespecs.
    
    * datix lib removed
    
    * Renamed as GraphicHelpers
    
    * mix format
    
    * graphic alias to PageView
    
    * Graphics functions refactored + docs
    
    * New structure: GraphicHelpers and Invoices.Graphics
    
    * mix format
    
    * Invoices.Statistics module finished
    
    * View and template finished
    
    * Small bugs
    
    * Forgotten doc
    
    * Changed way to pass day param to Statistics.get_data/1
    
    * Duplicate function definition

---
+++ lib/siwapp_web/views/graphic_helpers.ex
@@ -0,0 +1,22 @@
+defmodule SiwappWeb.GraphicHelpers do
+  alias Contex.{LinePlot, Plot, Dataset}
+
+  @doc """
+  Returns a SVG graphic of a line plot (500x200 size) with the given 'data'.
+
+  'data' must be a list of tuples with size of 2.
+  """
+  @spec line_plot([{any(), any()}]) :: {:safe, [...]}
+  def line_plot(data) do
+    options = [
+      data_labels: false,
+      default_style: false,
+      smoothed: false
+    ]
+
+    data
+    |> Dataset.new()
+    |> Plot.new(LinePlot, 500, 200, options)
+    |> Plot.to_svg()
+  end
+end
--- lib/siwapp_web/views/page_view.ex
+++ lib/siwapp_web/views/page_view.ex
@@ -1,3 +1,23 @@
 defmodule SiwappWeb.PageView do
   use SiwappWeb, :view
+
+  alias SiwappWeb.GraphicHelpers
+  alias Siwapp.Invoices
+
+  @doc """
+  Creates the dashboard chart with information about invoices (amounts of money per day).
+  """
+  @spec dashboard_chart :: {:safe, [...]}
+  def dashboard_chart() do
+    Invoices.Statistics.get_data()
+    |> date_to_Naive_type()
+    |> GraphicHelpers.line_plot()
+  end
+
+  # This is necessary because Contex.DateScale only accepts DateTime or NaiveDateTime types.
+  @spec date_to_Naive_type([map()]) :: [map()]
+  defp date_to_Naive_type(data) do
+    data
+    |> Enum.map(fn {date, amount} -> {NaiveDateTime.new!(date, ~T[00:00:00]), amount} end)
+  end
 end
