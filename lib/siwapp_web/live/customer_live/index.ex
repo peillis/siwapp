650c06bcb8d7ebe97f772ecc5a58cbd099e96f85
nurietzche <nuria.gallego@doofinder.com>
2021-12-27 09:35:56 +0100

    Refactor of customer editing handling

--- lib/siwapp_web/live/customer_live/index.ex
+++ lib/siwapp_web/live/customer_live/index.ex
@@ -6,10 +6,4 @@ defmodule SiwappWeb.CustomerLive.Index do
   def mount(_params, _session, socket) do
     {:ok, assign(socket, customers: Customers.list(), page_title: "Customers")}
   end
-
-  def path_edit_customer(id), do: "/customers/" <> to_string(id) <> "/edit"
-
-  def handle_event("edit", %{"id" => id}, socket) do
-    {:noreply, push_redirect(socket, to: Routes.customer_edit_path(socket, :edit, id))}
-  end
 end
