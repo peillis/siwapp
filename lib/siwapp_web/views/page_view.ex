7db996b9f4262c8fa9f9a1a7dead9d4f98356b42
Sergio Olivares <sergio.olivares@doofinder.com>
2021-12-27 10:12:52 +0100

    View and template finished

--- lib/siwapp_web/views/page_view.ex
+++ lib/siwapp_web/views/page_view.ex
@@ -1,5 +1,22 @@
 defmodule SiwappWeb.PageView do
   use SiwappWeb, :view
 
+  alias SiwappWeb.GraphicHelpers
   alias Siwapp.Invoices
+
+  @spec dashboard_chart :: {:safe, [...]}
+  def dashboard_chart() do
+    Invoices.Statistics.get_data()
+    |> date_to_Naive_type()
+    |> GraphicHelpers.line_plot()
+  end
+
+  # This is necessary because Contex.DateScale only accepts DateTime or NaiveDateTime types.
+  @spec date_to_Naive_type([map()]) :: [map()]
+  defp date_to_Naive_type(data) do
+    data
+    |> Enum.map(fn %{issue_date: x} = day_data ->
+      %{day_data | issue_date: NaiveDateTime.new!(x, ~T[00:00:00])}
+    end)
+  end
 end
