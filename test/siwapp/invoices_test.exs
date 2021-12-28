10e2e21f8b1c43487fb4cb399f2154f28fadcc6c
Sergio Olivares <sergio.olivares@doofinder.com>
2021-12-28 12:11:26 +0100

    Test finished

--- test/siwapp/invoices_test.exs
+++ test/siwapp/invoices_test.exs
@@ -1,9 +1,50 @@
 defmodule Siwapp.InvoicesTest do
   use Siwapp.DataCase
 
-  alias Siwapp.Invoices
+  alias Siwapp.Invoices.Invoice
+  alias Siwapp.Customers
+  alias Siwapp.Series
 
+  describe "saving restrictions" do
+    test "not draft cannot be saved if does not have an associated series" do
+      {:ok, c} = Customers.create(%{name: "James"})
 
+      changeset =
+        Invoice.changeset(%Invoice{}, %{
+          name: "Melissa",
+          customer_id: c.id,
+          draft: false
+        })
 
+      assert %{series_id: ["can't be blank"]} = errors_on(changeset)
+    end
 
+    test "not draft can be saved if has an associated series" do
+      {:ok, c} = Customers.create(%{name: "Melissa"})
+      {:ok, s} = Series.create(%{value: "L-"})
+
+      changeset =
+        Invoice.changeset(%Invoice{}, %{
+          name: "Melissa",
+          customer_id: c.id,
+          series_id: s.id,
+          draft: false
+        })
+
+      assert changeset.valid?
+    end
+
+    test "draft can be saved if does not have an associated series" do
+      {:ok, c} = Customers.create(%{name: "Mery"})
+
+      changeset =
+        Invoice.changeset(%Invoice{}, %{
+          name: "Melissa",
+          customer_id: c.id,
+          draft: true
+        })
+
+      assert changeset.valid?
+    end
+  end
 end
