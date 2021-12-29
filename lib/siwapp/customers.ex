787f735a7a974023648e2a3fca137de4d6dfdf68
peillis <enrique@doofinder.com>
2021-12-29 13:28:14 +0100

    rf some customer fns

--- lib/siwapp/customers.ex
+++ lib/siwapp/customers.ex
@@ -35,7 +35,26 @@ defmodule Siwapp.Customers do
   """
   def get!(id), do: Repo.get!(Customer, id)
 
+  def get(identification, name) do
+    case Repo.get_by(Customer, identification: identification) do
+      nil ->
+        get_by_hash_id(identification, name)
+
+      customer ->
+        customer
+    end
+  end
+
   def change(%Customer{} = customer, attrs \\ %{}) do
     Customer.changeset(customer, attrs)
+  end
+
+  defp get_by_hash_id(identification, name) do
+    hash_id = Customer.create_hash_id(identification, name)
+
+    case Repo.get_by(Customer, hash_id: hash_id) do
+      nil -> nil
+      customer -> customer
+    end
   end
 end
