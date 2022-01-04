f4fe7cd6da578113db07317bbb3708d3e38fad3a
peillis <enrique@doofinder.com>
2022-01-04 17:55:44 +0100

    rf item

--- lib/siwapp/invoices/item.ex
+++ lib/siwapp/invoices/item.ex
@@ -41,27 +41,25 @@ defmodule Siwapp.Invoices.Item do
     unitary_cost = get_field(changeset, :unitary_cost)
     discount = get_field(changeset, :discount)
 
-    changeset
-    |> put_change(:net_amount, quantity * unitary_cost - quantity * unitary_cost * discount / 100)
+    net_amount = quantity * unitary_cost - quantity * unitary_cost * discount / 100
+
+    put_change(changeset, :net_amount, net_amount)
   end
 
   def set_taxes_amount(changeset) do
-    taxes = get_field(changeset, :taxes)
+    case get_field(changeset, :taxes) do
+      [] ->
+        changeset
 
-    if taxes == [] do
-      changeset
-    else
-      changeset
-      |> put_change(:taxes_amount, taxes_amount(changeset))
-    end
-  end
+      taxes ->
+        net_amount = get_field(changeset, :net_amount)
 
-  defp taxes_amount(changeset) do
-    net_amount = get_field(changeset, :net_amount)
-    taxes = get_field(changeset, :taxes)
+        taxes_amounts =
+          for tax <- taxes, into: %{} do
+            {tax.id, tax.value * net_amount / 100}
+          end
 
-    for tax <- taxes, id = tax.id, value = tax.value, into: %{} do
-      {id, value * net_amount / 100}
+        put_change(changeset, :taxes_amount, taxes_amounts)
     end
   end
 end
