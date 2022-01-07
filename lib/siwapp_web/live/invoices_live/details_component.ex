182bb2b8a5d828eadf82c341d6790aebb193e568
Sergio Olivares <sergio.olivares@doofinder.com>
2022-01-07 14:09:25 +0100

    All components working staticly

---
+++ lib/siwapp_web/live/invoices_live/details_component.ex
@@ -0,0 +1,4 @@
+defmodule SiwappWeb.InvoicesLive.DetailsComponent do
+  @moduledoc false
+  use SiwappWeb, :live_component
+end
---
+++ lib/siwapp_web/live/invoices_live/payments_component.ex
@@ -0,0 +1,4 @@
+defmodule SiwappWeb.InvoicesLive.PaymentsComponent do
+  @moduledoc false
+  use SiwappWeb, :live_component
+end
