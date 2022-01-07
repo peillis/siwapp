1824a19ecc556e1cc4cc44f158215136ab99fc65
nurietzche <nuria.gallego@doofinder.com>
2022-01-07 15:04:56 +0100

    Controller and template for settings I left the old version commented just in case we find this more suitable in the future

--- lib/siwapp_web/controllers/settings_controller.ex
+++ lib/siwapp_web/controllers/settings_controller.ex
@@ -1,37 +1,66 @@
 defmodule SiwappWeb.SettingsController do
   use SiwappWeb, :controller
 
-  alias Siwapp.Settings
+  #alias Siwapp.Settings
   alias Siwapp.SettingsForm
 
+  plug :assign_changeset_and_labels
+
   def edit(conn, _params) do
-    settings = Settings.prepare_current_settings()
-    changeset = SettingsForm.changeset(%{})
-    IO.inspect "settings"
-    IO.inspect settings
-    IO.inspect "changeset"
-    IO.inspect changeset
     conn
-    |> assign(:changeset, changeset )
-    |> assign(:settings,settings )
     |> render("edit.html")
   end
 
-  def update(conn, params) do
-    settings = Settings.prepare_current_settings()
-    changeset = SettingsForm.changeset(%{})
+  def update(conn, %{"form" => attrs} = params) do
+    changeset = conn.assigns.changeset
     IO.inspect params
     conn
-    |> assign(:settings, settings)
-    |> assign(:changeset, changeset)
-    |> render("edit.html")
+    case SettingsForm.apply_user_settings(changeset, attrs) do
+      {:ok, _applied_form} ->
+        conn
+        |> put_flash(:info, "Settings saved succesfully")
+        |> render("edit.html")
+      {:error, changeset} ->
+        render(conn, "edit.html", changeset: changeset)
+    end
+  end
+
+  def update(conn, %{"action" => "update_email"} = params) do
+    %{"current_password" => password, "user" => user_params} = params
+    user = conn.assigns.current_user
+
+    case Accounts.apply_user_email(user, password, user_params) do
+      {:ok, applied_user} ->
+        Accounts.deliver_update_email_instructions(
+          applied_user,
+          user.email,
+          &Routes.user_settings_url(conn, :confirm_email, &1)
+        )
+
+        conn
+        |> put_flash(
+          :info,
+          "A link to confirm your email change has been sent to the new address."
+        )
+        |> redirect(to: Routes.user_settings_path(conn, :edit))
+
+      {:error, changeset} ->
+        render(conn, "edit.html", email_changeset: changeset)
+    end
+  end
+  
+  defp assign_changeset_and_labels(conn, _opts) do
+    labels = SettingsForm.get_labels
+    conn
+    |> assign(:labels, labels)
+    |> assign(:changeset, SettingsForm.change())
   end
 
   """
   def edit(conn, _params) do
     conn
     |> assign(:settings, Settings.prepare_current_settings())
-    |> render("edit.html")
+    |> render("edit._old.html")
   end
 
   def update(conn, %{"key_values" => key_values_map}) do
@@ -39,7 +68,7 @@ defmodule SiwappWeb.SettingsController do
 
     conn
     |> assign(:settings, Settings.prepare_current_settings())
-    |> render("edit.html")
+    |> render("edit_old.html")
   end
 
   def act(key, value) do
