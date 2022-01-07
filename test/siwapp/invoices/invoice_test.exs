1e77b30c8a6dd03218fcc4952e2e78cb2e41fa2e
Sergio Olivares <49884623+olirods@users.noreply.github.com>
2022-01-07 14:21:55 +0100

    Draft limitations TDD (#123)
    * Test finished
    
    * New condition changed
    
    * Test working, fixes #95
    
    * Comments added
    
    * Small fix

--- test/siwapp/invoices/invoice_test.exs
+++ test/siwapp/invoices/invoice_test.exs
@@ -1,7 +1,10 @@
 defmodule Siwapp.InvoiceTest do
   use Siwapp.DataCase
 
+  alias Siwapp.Invoices
   alias Siwapp.Invoices.Invoice
+
+  alias Siwapp.Commons
 
   describe "Saving restrictions, and draft exception" do
     test "An invoice cannot be saved if does not have required fields" do
@@ -26,6 +29,30 @@ defmodule Siwapp.InvoiceTest do
       changeset = Invoice.changeset(%Invoice{}, %{name: "Melissa", draft: true})
 
       assert changeset.valid?
+    end
+  end
+
+  describe "Limited draft enablement" do
+    test "An existing regular invoice cannot be converted to draft" do
+      {:ok, series} = Commons.create_series(%{name: "A-Series", value: "A-"})
+
+      {:ok, invoice} =
+        Invoices.create(%{name: "Melissa", series_id: series.id, issue_date: Date.utc_today()})
+
+      changeset = Invoice.changeset(invoice, %{draft: true})
+
+      assert %{draft: ["can't be enabled, invoice is not new"]} = errors_on(changeset)
+    end
+
+    test "An existing draft can be re-marked as draft" do
+      {:ok, invoice} = Invoices.create(%{name: "Melissa", draft: true})
+      changeset = Invoice.changeset(invoice, %{draft: true})
+
+      assert changeset.valid?
+    end
+
+    test "A new invoice can be saved as draft" do
+      assert {:ok, %Invoice{}} = Invoices.create(%{name: "Melissa", draft: true})
     end
   end
 end
