b4e2d73b508b7ae70a90ca0e3b5bf97939c08a41
Pabloj20 <pablo.rubio@doofinder.com>
2022-01-07 10:56:23 +0100

    mix format

--- lib/siwapp_web/views/layout_view.ex
+++ lib/siwapp_web/views/layout_view.ex
@@ -8,16 +8,32 @@ defmodule SiwappWeb.LayoutView do
   def shared_button(conn) do
     case conn.request_path do
       n when n in ["/invoices", "/invoices/edit", "/users/settings", "/"] ->
-        button("New Invoice", to: Routes.page_path(conn, :edit_invoices), method: :get, class: "button is-primary")
+        button("New Invoice",
+          to: Routes.page_path(conn, :edit_invoices),
+          method: :get,
+          class: "button is-primary"
+        )
 
       n when n in ["/series", "/series/new"] ->
-        button("New Series", to: Routes.series_index_path(conn, :new), method: :get, class: "button is-primary")
+        button("New Series",
+          to: Routes.series_index_path(conn, :new),
+          method: :get,
+          class: "button is-primary"
+        )
 
       n when n in ["/taxes", "/taxes/new"] ->
-        button("New Tax", to: Routes.taxes_index_path(conn, :new), method: :get, class: "button is-primary")
+        button("New Tax",
+          to: Routes.taxes_index_path(conn, :new),
+          method: :get,
+          class: "button is-primary"
+        )
 
       n when n in ["/templates", "/templates/new"] ->
-        button("New Template", to: Routes.templates_edit_path(conn, :new), method: :get, class: "button is-primary")
+        button("New Template",
+          to: Routes.templates_edit_path(conn, :new),
+          method: :get,
+          class: "button is-primary"
+        )
     end
   end
 end
