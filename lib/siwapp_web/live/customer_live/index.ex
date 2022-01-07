8695b2f195ba154314a74c402f1e800020d1974f
nurietzche <nuria.gallego@doofinder.com>
2022-01-07 11:32:41 +0100

    Avoiding credo warnings

--- lib/siwapp_web/live/customer_live/index.ex
+++ lib/siwapp_web/live/customer_live/index.ex
@@ -3,6 +3,11 @@ defmodule SiwappWeb.CustomerLive.Index do
 
   alias Siwapp.Customers
 
+  @moduledoc """
+
+  This module manages the customer index view
+  """
+
   def mount(_params, _session, socket) do
     {:ok, assign(socket, customers: Customers.list(), page_title: "Customers")}
   end
