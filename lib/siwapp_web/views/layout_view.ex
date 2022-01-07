9be98e069502b5530de170b46b88af8e4c30d27f
Pabloj20 <pablo.rubio@doofinder.com>
2022-01-07 10:30:27 +0100

    create  a fn to use at the _shared_button template

--- lib/siwapp_web/views/layout_view.ex
+++ lib/siwapp_web/views/layout_view.ex
@@ -4,4 +4,20 @@ defmodule SiwappWeb.LayoutView do
   # Phoenix LiveDashboard is available only in development by default,
   # so we instruct Elixir to not warn if the dashboard route is missing.
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
+
+  def redirect_path(request_path) do
+    case request_path do
+      n when n in ["/invoices", "/invoices/edit", "/users/settings", "/"] ->
+        :invoices
+
+      n when n in ["/series", "/series/new"] ->
+        :series
+
+      n when n in ["/taxes", "/taxes/new"] ->
+        :taxes
+
+      n when n in ["/templates", "/templates/new"] ->
+        :templates
+    end
+  end
 end
