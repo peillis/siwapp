81d77953607120373cf4fb538ca60026ff59c6c6
Pabloj20 <66000403+Pabloj20@users.noreply.github.com>
2022-01-10 09:49:51 +0100

    Pablo/recurring invoices (#136)
    * Recurring Invoices LiveView
    
    * mix format
    
    * renaming fn from sing_or_plur to singular_or_plural. Fix some problems with the css

--- lib/siwapp_web/live/live_helpers.ex
+++ lib/siwapp_web/live/live_helpers.ex
@@ -13,4 +13,20 @@ defmodule SiwappWeb.LiveHelpers do
     modal_opts = [id: :modal, return_to: path, component: component, opts: opts]
     live_component(SiwappWeb.ModalComponent, modal_opts)
   end
+
+  def type_of_period(period_type, period) do
+    case period_type do
+      "Daily" -> singular_or_plural(period, "day")
+      "Monthly" -> singular_or_plural(period, "month")
+      "Yearly" -> singular_or_plural(period, "year")
+    end
+  end
+
+  defp singular_or_plural(period, str) do
+    if period > 1 do
+      str <> "s"
+    else
+      str
+    end
+  end
 end
