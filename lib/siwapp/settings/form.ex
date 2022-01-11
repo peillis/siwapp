2ffa079a78b3654eb9577e2dde5e65c10d0ba399
nurietzche <nuria.gallego@doofinder.com>
2022-01-11 13:22:14 +0100

    Definición struct Form para SettingsForm

---
+++ lib/siwapp/settings/form.ex
@@ -0,0 +1,44 @@
+defmodule Siwapp.Settings.Form do
+  import Ecto.Changeset
+
+  @moduledoc false
+  @labels [
+    :company,
+    :company_vat_id,
+    :company_address,
+    :company_phone,
+    :company_email,
+    :company_website,
+    :company_logo,
+    :currency,
+    :legal_terms,
+    :days_to_due
+  ]
+
+  @types [
+    :string,
+    :string,
+    :string,
+    :integer,
+    :string,
+    :string,
+    :string,
+    :string,
+    :string,
+    :integer
+  ]
+
+  defstruct @labels
+
+  def changeset(form, attrs \\ %{}) do
+    {form, fields_map}
+    |> cast(attrs, Map.keys(fields_map))
+    |> validate_format(:company_email, ~r/@/)
+    # Lista de ejemplo, ya se incluirán todas como constantes
+    |> validate_inclusion(:currency, ["USD", "EUR"])
+  end
+
+  def labels, do: @labels
+  def pairs, do: Enum.zip(@labels, @types)
+  defp fields_map, do: Map.new(pairs)
+end
