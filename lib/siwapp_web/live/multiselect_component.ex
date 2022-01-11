14d1a667048d7c203b309a943f7be39613a923df
peillis <enrique@doofinder.com>
2022-01-11 19:04:50 +0100

    wip

--- lib/siwapp_web/live/multiselect_component.ex
+++ lib/siwapp_web/live/multiselect_component.ex
@@ -8,68 +8,52 @@ defmodule SiwappWeb.MultiselectComponent do
   def update(assigns, socket) do
     socket =
       socket
-      |> assign(selected: [])
-      |> assign(not_selected: [])
-      |> assign(options: assigns.options)
+      |> assign(selected: MapSet.new())
+      |> assign(options: MapSet.new(assigns.options))
     {:ok, socket}
   end
 
   def render(assigns) do
     ~H"""
     <div class="msa-wrapper">
-      <label for="msa-input">Choose some cars:</label>
-      <input type="hidden" id="msa-input">
+      <%= for {_k, v} <- @selected do %>
+        <input type="hidden" value={v}>
+      <% end %>
       <div class="input-presentation" phx-click={JS.toggle(to: "#tag-list")}>
         <span class="placeholder">Select Tags</span>
-        <div class="tag-badge">
-          <span>saab</span>
-          <button>x</button>
-        </div>
-        <div class="tag-badge">
-          <span>Seat</span>
-          <button>x</button>
-        </div>
+        <%= for {k, v} <- @selected do %>
+          <div class="tag-badge">
+            <span><%= k %></span>
+            <button type="button" phx-click={JS.push("remove", target: @myself, value: %{key: k, val: v})}>x</button>
+          </div>
+        <% end %>
       </div>
       <ul id="tag-list" style="display: none;">
-        <%= for {k, v} <- @options do %>
-        <li><%= k %></li>
+        <%= for {k, v} <- not_selected(@options, @selected) do %>
+          <li phx-click={JS.push("add", target: @myself, value: %{key: k, val: v}) |> JS.toggle(to: "#tag-list")}><%= k %></li>
         <% end %>
       </ul>
     </div>
     """
   end
 
-  def handle_event("remove", %{"key" => key}, socket) do
-    new = Map.delete(socket.assigns.meta_attributes, key)
-    send_update(__MODULE__, id: "meta_attributes", meta_attributes: new)
+  def handle_event("remove", %{"key" => key, "val" => value}, socket) do
+    {key, value} = convert({key, value})
 
-    {:noreply, socket}
+    {:noreply, update(socket, :selected, &MapSet.delete(&1, {key, value}))}
   end
 
-  def handle_event("changing-key", %{"value" => value}, socket) do
-    {:noreply, assign(socket, :new_key, value)}
+  def handle_event("add", %{"key" => key, "val" => value}, socket) do
+    {key, value} = convert({key, value})
+
+    {:noreply, update(socket, :selected, &MapSet.put(&1, {key, value}))}
   end
 
-  def handle_event("changing-value", %{"value" => value}, socket) do
-    {:noreply, assign(socket, :new_value, value)}
+  defp convert({key, value}) do
+    {String.to_atom(key), value}
   end
 
-  def handle_event("add", _params, %{assigns: assigns} = socket) do
-    new =
-      case assigns.new_key do
-        "" ->
-          assigns.meta_attributes
-
-        _ ->
-          Map.put(
-            assigns.meta_attributes,
-            assigns.new_key,
-            assigns.new_value
-          )
-      end
-
-    send_update(__MODULE__, id: "meta_attributes", meta_attributes: new)
-
-    {:noreply, socket}
+  defp not_selected(options, selected) do
+    MapSet.difference(options, selected)
   end
 end
