2395d63e5b02c890f9a7c929afa32b748345d70c
nurietzche <nuria.gallego@doofinder.com>
2022-01-12 13:46:50 +0100

    Type RecurringInvoice definido en boundary Ya se puede usar el tipo RecurringInvoice.t

--- lib/siwapp/recurring_invoices.ex
+++ lib/siwapp/recurring_invoices.ex
@@ -7,36 +7,46 @@ defmodule Siwapp.RecurringInvoices do
   alias Siwapp.RecurringInvoices.RecurringInvoice
   alias Siwapp.Repo
 
+  @spec list :: [RecurringInvoice.t()]
   def list do
     # query = Query.invoices()
     Repo.all(RecurringInvoice)
   end
 
+  @spec get!(pos_integer()) :: RecurringInvoice.t()
   def get!(id), do: Repo.get!(RecurringInvoice, id)
 
+  @spec get!(pos_integer(), :preload) ::RecurringInvoice.t()
   def get!(id, :preload),
     do: Repo.get!(RecurringInvoice, id) |> Repo.preload([:customer, :series])
 
+  @spec create(map) :: {:ok, RecurringInvoice.t()} | {:error, Ecto.Changeset.t()}
   def create(attrs \\ %{}) do
     %RecurringInvoice{}
     |> RecurringInvoice.changeset(attrs)
     |> Repo.insert()
   end
 
+  @spec update(RecurringInvoice.t(), map) ::
+          {:ok, RecurringInvoice.t()} | {:error, Ecto.Changeset.t()}
   def update(recurring_invoice, attrs) do
     recurring_invoice
     |> RecurringInvoice.changeset(attrs)
     |> Repo.update()
   end
 
+  @spec delete(RecurringInvoice.t() | Ecto.Changeset.t()) ::
+          {:ok, RecurringInvoice.t()} | {:error, Ecto.Changeset.t()}
   def delete(recurring_invoice) do
     Repo.delete(recurring_invoice)
   end
 
+  # Expect will be added when function is finished
   def generate_invoices(id) do
     Repo.get!(RecurringInvoice, id)
   end
 
+  @spec change(RecurringInvoice.t(), map) :: Ecto.Changeset.t()
   def change(%RecurringInvoice{} = recurring_invoice, attrs \\ %{}) do
     RecurringInvoice.changeset(recurring_invoice, attrs)
   end
