36285ef92c0d7245d7fdaa256c3b008e650f6f25
nurietzche <nuria.gallego@doofinder.com>
2022-01-12 10:39:10 +0100

    Página funcionando con almacenamiento en database Falta ponerlo bonito

---
+++ lib/siwapp_web/views/settings_view.ex
@@ -0,0 +1,15 @@
+defmodule SiwappWeb.SettingsView do
+  use SiwappWeb, :view
+
+  def adequate_input(label, type, f) do
+    cond do
+      to_string(label) =~ "email" -> email_input(f, label)
+      to_string(label) =~ "phone" -> telephone_input(f, label)
+      # Currency list to be updated
+      to_string(label) =~ "currency" -> select(f, label, ["USD", "EUR"])
+      to_string(label) =~ "address" || to_string(label) =~ "terms" -> textarea(f, label)
+      type == :integer -> number_input(f, label)
+      type == :string -> text_input(f, label)
+    end
+  end
+end
