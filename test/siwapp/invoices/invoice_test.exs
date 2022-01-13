eeeaac057065e17784026923d1143e974449e833
Sergio Olivares <49884623+olirods@users.noreply.github.com>
2022-01-13 15:43:48 +0100

    rf invoices test (#169)
    * New invoice fixtures created
    
    * Invoices tests adapted to fixtures
    
    * mix tasks
    
    * code readibility small improvements

--- test/siwapp/invoices/invoice_test.exs
+++ test/siwapp/invoices/invoice_test.exs
@@ -1,20 +1,28 @@
 defmodule Siwapp.InvoiceTest do
   use Siwapp.DataCase
 
+  alias Siwapp.Commons
   alias Siwapp.Invoices
   alias Siwapp.Invoices.Invoice
 
-  alias Siwapp.Commons
+  import Siwapp.InvoicesFixtures
 
-  describe "Saving restrictions, and draft exception" do
-    test "An invoice cannot be saved if does not have required fields" do
+  setup do
+    {:ok, series} = Commons.create_series(%{name: "A-Series", code: "A-"})
+    {:ok, tax1} = Commons.create_tax(%{name: "VAT", value: 21, default: true})
+    {:ok, tax2} = Commons.create_tax(%{name: "RETENTION", value: -15})
+    %{series_id: series.id, taxes: [tax1, tax2]}
+  end
+
+  describe "saving restrictions, and draft exception: " do
+    test "an invoice cannot be saved if does not have required fields" do
       changeset = Invoice.changeset(%Invoice{}, %{name: "Melissa"})
 
       assert %{series_id: ["can't be blank"]} = errors_on(changeset)
       assert %{issue_date: ["can't be blank"]} = errors_on(changeset)
     end
 
-    test "An invoice can be saved if it has all the required fields" do
+    test "an invoice can be saved if it has all the required fields" do
       changeset =
         Invoice.changeset(%Invoice{}, %{
           name: "Melissa",
@@ -25,101 +33,37 @@ defmodule Siwapp.InvoiceTest do
       assert changeset.valid?
     end
 
-    test "A draft is valid always" do
+    test "a draft is valid always" do
       changeset = Invoice.changeset(%Invoice{}, %{name: "Melissa", draft: true})
 
       assert changeset.valid?
     end
   end
 
-  describe "Limited draft enablement" do
-    test "An existing regular invoice cannot be converted to draft" do
-      {:ok, series} = Commons.create_series(%{name: "A-Series", code: "A-"})
-
-      {:ok, invoice} =
-        Invoices.create(%{name: "Melissa", series_id: series.id, issue_date: Date.utc_today()})
-
-      changeset =
-        Repo.preload(invoice, :items)
-        |> Invoice.changeset(%{draft: true})
+  describe "limited draft enablement: " do
+    test "an existing regular invoice cannot be converted to draft" do
+      invoice = invoice_fixture(draft: false)
+      changeset = Invoice.changeset(invoice, %{draft: true})
 
       assert %{draft: ["can't be enabled, invoice is not new"]} = errors_on(changeset)
     end
 
-    test "An existing draft can be re-marked as draft" do
-      {:ok, invoice} = Invoices.create(%{name: "Melissa", draft: true})
-
-      changeset =
-        Repo.preload(invoice, :items)
-        |> Invoice.changeset(%{draft: true})
+    test "an existing draft can be re-marked as draft" do
+      invoice = invoice_fixture(draft: true)
+      changeset = Invoice.changeset(invoice, %{draft: true})
 
       assert changeset.valid?
     end
 
-    test "A new invoice can be saved as draft" do
-      assert {:ok, %Invoice{}} = Invoices.create(%{name: "Melissa", draft: true})
+    test "a new invoice can be saved as draft" do
+      assert {:ok, %Invoice{}} = Invoices.create(valid_invoice_attributes(%{draft: true}))
     end
   end
 
-  describe "Total amounts for an invoice" do
-    test "Total net amount of an invoice without items is 0" do
-      {:ok, invoice} = Invoices.create(%{name: "Melissa", draft: true})
-
-      assert invoice.net_amount == 0
-    end
-
-    test "Total taxes amounts of an invoice without items is an empty map" do
-      {:ok, invoice} = Invoices.create(%{name: "Melissa", draft: true})
-
-      assert invoice.taxes_amounts == %{}
-    end
-
-    test "Total taxes amounts of an invoice whose items don't have any taxes associated is an empty map" do
-      {:ok, invoice} = Invoices.create(%{name: "Melissa", draft: true})
-
-      Invoices.create_item(invoice, %{quantity: 1, unitary_cost: 133, discount: 10})
-
-      assert invoice.taxes_amounts == %{}
-    end
-
-    test "Total gross amount of an invoice without items is 0" do
-      {:ok, invoice} = Invoices.create(%{name: "Melissa", draft: true})
-
-      assert invoice.gross_amount == 0
-    end
-
-    test "Total net amount is the rounded sum of the net amounts of each item" do
-      {:ok, invoice} =
-        Invoices.create(%{
-          name: "Melissa",
-          draft: true,
-          items: [
-            %{
-              quantity: 1,
-              unitary_cost: 133,
-              discount: 10
-            },
-            %{
-              quantity: 1,
-              unitary_cost: 133,
-              discount: 10
-            }
-          ]
-        })
-
-      # 133-(133*(10/100)) = 119.70 (net_amount per item)
-      # 119.70*2 = 239.40 (total net_amount)
-      assert invoice.net_amount == 239
-    end
-
-    test "Total taxes amounts is a map with the total amount per tax" do
-      Commons.create_tax(%{name: "VAT", value: 21, default: true})
-      Commons.create_tax(%{name: "RETENTION", value: -15})
-
-      {:ok, invoice} =
-        Invoices.create(%{
-          name: "Melissa",
-          draft: true,
+  describe "total amounts for an invoice: " do
+    setup do
+      invoice =
+        invoice_fixture(
           items: [
             %{
               quantity: 1,
@@ -134,38 +78,56 @@ defmodule Siwapp.InvoiceTest do
               taxes: ["VAT", "RETENTION"]
             }
           ]
-        })
+        )
 
+      %{invoice: invoice}
+    end
+
+    test "total net amount of an invoice without items is 0" do
+      invoice = invoice_fixture(items: [])
+
+      assert invoice.net_amount == 0
+    end
+
+    test "total taxes amounts of an invoice without items is an empty map" do
+      invoice = invoice_fixture(items: [])
+
+      assert invoice.taxes_amounts == %{}
+    end
+
+    test "total taxes amounts of an invoice whose items don't have any taxes associated is an empty map" do
+      invoice =
+        invoice_fixture(
+          items: [valid_item_attributes(taxes: []), valid_item_attributes(taxes: [])]
+        )
+
+      assert invoice.taxes_amounts == %{}
+    end
+
+    test "total gross amount of an invoice without items is 0" do
+      invoice = invoice_fixture(items: [])
+
+      assert invoice.gross_amount == 0
+    end
+
+    test "Total net amount is the rounded sum of the net amounts of each item", %{
+      invoice: invoice
+    } do
+      # 133-(133*(10/100)) = 119.70 (net_amount per item)
+      # 119.70*2 = 239.40 (total net_amount)
+      assert invoice.net_amount == 239
+    end
+
+    test "Total taxes amounts is a map with the total amount per tax", %{invoice: invoice} do
       # 119.70 (net_amount per item)
       # 119.70*(21/100) = 25.137 (VAT per item)
       # 119.70*(-15/100) = -17.955 (RETENTION for 2nd item)
       assert invoice.taxes_amounts == %{"RETENTION" => -17.955, "VAT" => 50.274}
     end
 
-    test "Total gross amount is the rounded sum of the total net amount and the taxes amounts" do
-      Commons.create_tax(%{name: "VAT", value: 21, default: true})
-      Commons.create_tax(%{name: "RETENTION", value: -15})
-
-      {:ok, invoice} =
-        Invoices.create(%{
-          name: "Melissa",
-          draft: true,
-          items: [
-            %{
-              quantity: 1,
-              unitary_cost: 133,
-              discount: 10,
-              taxes: ["VAT"]
-            },
-            %{
-              quantity: 1,
-              unitary_cost: 133,
-              discount: 10,
-              taxes: ["VAT", "RETENTION"]
-            }
-          ]
-        })
-
+    test "Total gross amount is the rounded sum of the total net amount and the taxes amounts", %{
+      invoice: invoice
+    } do
       # 239 (total net_amount)
       # %{"RETENTION" => -17.955, "VAT" => 50.274} (total taxes amounts)
       # 239 + 50.274 - 17.955 = 271.319 (total gross_amount)
