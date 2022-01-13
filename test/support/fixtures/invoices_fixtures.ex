352bb1d0cdf9eb4ca246b2f89a1a77e3ef180b7b
Sergio Olivares <sergio.olivares@doofinder.com>
2022-01-13 13:31:17 +0100

    New invoice fixtures created

---
+++ test/support/fixtures/invoices_fixtures.ex
@@ -0,0 +1,41 @@
+defmodule Siwapp.InvoicesFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `Siwapp.Invoices` context.
+  """
+
+  alias Siwapp.CustomersFixtures
+  alias Siwapp.Invoices
+  alias Siwapp.Repo
+  alias Siwapp.Commons
+
+  def valid_item_attributes(attrs \\ %{}) do
+    IO.inspect Commons.list_taxes()
+
+    Enum.into(attrs, %{
+      quantity: 1,
+      unitary_cost: 133,
+      discount: 10,
+      taxes: ["VAT", "RETENTION"]
+    })
+  end
+
+  def valid_invoice_attributes(attrs \\ %{}) do
+    Enum.into(attrs, %{
+      name: CustomersFixtures.unique_customer_name(),
+      identification: CustomersFixtures.unique_customer_identification(),
+      series_id: hd(Commons.list_series()).id,
+      issue_date: Date.utc_today(),
+      items: [valid_item_attributes()]
+    })
+  end
+
+  def invoice_fixture(attrs \\ %{}) do
+    {:ok, invoice} =
+      attrs
+      |> valid_invoice_attributes()
+      |> Invoices.create()
+
+    Repo.preload(invoice, [:customer, {:items, :taxes}, :series])
+  end
+end
