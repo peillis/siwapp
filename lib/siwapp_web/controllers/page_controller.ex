af086dc5df26a4c1fec3a3bbee5139546492b3a0
Pabloj20 <pablo.rubio@doofinder.com>
2022-01-18 13:07:59 +0100

    Initial version of show_invoice

---
+++ lib/siwapp_web/controllers/page_controller.ex
@@ -0,0 +1,13 @@
+defmodule SiwappWeb.PageController do
+  use SiwappWeb, :controller
+  alias Siwapp.Invoices
+  def show_invoice(conn, %{"id" => id}) do
+    invoice = Invoices.get!(String.to_integer(id))
+
+    conn =
+      conn
+      |> assign(:invoice, invoice)
+
+    render(conn, "show_invoice.html")
+  end
+end
