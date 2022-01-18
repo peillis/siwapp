54132121aa9427c91fcabe958fcbefc97dcf7d78
Pabloj20 <pablo.rubio@doofinder.com>
2022-01-18 13:07:59 +0100

    Initial version of show_invoice

--- lib/siwapp_web/controllers/iframe_controller.ex
+++ lib/siwapp_web/controllers/iframe_controller.ex
@@ -1,17 +1,20 @@
 defmodule SiwappWeb.IframeController do
   use SiwappWeb, :controller
   alias Siwapp.Invoices
+  alias Siwapp.Templates
 
   plug :put_root_layout, false
   plug :put_layout, false
 
   def iframe(conn, %{"id" => id}) do
     invoice = Invoices.get!(String.to_integer(id), :preload)
+    template = Templates.get(:print_default).template
+    File.write("#{__DIR__}/../templates/iframe/default.html.heex", template)
 
     conn =
       conn
       |> assign(:invoice, invoice)
 
-    render(conn, "print_default.html")
+    render(conn, "default.html")
   end
 end
