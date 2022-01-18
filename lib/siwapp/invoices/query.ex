def37ebf8a0fb1b5e55a4171cec8d9ce13570d4d
Rodrigo Molina <rodrigomolina@doofinder.com>
2022-01-18 12:47:50 +0100

    resolviendo conflictos 2

--- lib/siwapp/invoices/query.ex
+++ lib/siwapp/invoices/query.ex
@@ -2,14 +2,10 @@ defmodule Siwapp.Invoices.Query do
   @moduledoc """
   Invoices Querys
   """
-  alias Siwapp.Invoices.Invoice
-
   import Ecto.Query
 
-  def list_preload do
-    from i in Invoice,
-      join: c in assoc(i, :customer),
-      preload: [customer: c]
+  def list_preload(query, term) do
+    preload(query, ^term)
   end
 
   def paginate(query, page, per_page) do
@@ -20,17 +16,14 @@ defmodule Siwapp.Invoices.Query do
     |> offset(^offset_by)
   end
 
-  # def scroll_list_query(page, per_page \\ 20) do
-  #   from(c in Invoice) |> paginate(page, per_page)
-  # end
-
   def scroll_list_query(query, page, per_page \\ 20) do
     query
     |> paginate(page, per_page)
   end
 
-  def by(field, value) do
-    where(Invoice, ^[{field, value}])
+  def by(query, field, value) do
+    query
+    |> where(^[{field, value}])
   end
 
   def list_past_due_or_pending(query) do
@@ -40,26 +33,23 @@ defmodule Siwapp.Invoices.Query do
     |> where(failed: false)
   end
 
-  def with_terms(terms) do
-    from(i in Invoice,
-      join: it in Siwapp.Invoices.Item,
-      where: ilike(it.description, ^"%#{terms}%"),
-      or_where: ilike(i.email, ^"%#{terms}%"),
-      or_where: ilike(i.name, ^"%#{terms}%"),
-      or_where: ilike(i.identification, ^"%#{terms}%")
-    )
+  def with_terms(query, terms) do
+    query
+    |> join(:inner, [i], it in Siwapp.Invoices.Item)
+    |> where([i, it], ilike(it.description, ^"%#{terms}%"))
+    |> or_where([i], ilike(i.email, ^"%#{terms}%"))
+    |> or_where([i], ilike(i.name, ^"%#{terms}%"))
+    |> or_where([i], ilike(i.identification, ^"%#{terms}%"))
   end
 
-  def issue_date_gteq(date) do
-    from(i in Invoice,
-      where: i.issue_date >= ^date
-    )
+  def issue_date_gteq(query, date) do
+    query
+    |> where([i], i.issue_date >= ^date)
   end
 
-  def issue_date_lteq(date) do
-    from(i in Invoice,
-      where: i.issue_date <= ^date
-    )
+  def issue_date_lteq(query, date) do
+    query
+    |> where([i], i.issue_date <= ^date)
   end
 
   @spec last_number_with_series_id(pos_integer()) :: Ecto.Query.t()
