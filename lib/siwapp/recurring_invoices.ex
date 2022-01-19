0f0b68529abdec3c14f8d5cc6fa2b38f125d218e
nurietzche <nuria.gallego@doofinder.com>
2022-01-19 15:45:02 +0100

    Función para cuántas invoices hay que generar Tanto hoy como en la finishing_date (si existen) se generan facturas y, por tanto, se consideran en el cómputo

--- lib/siwapp/recurring_invoices.ex
+++ lib/siwapp/recurring_invoices.ex
@@ -62,22 +62,67 @@ defmodule Siwapp.RecurringInvoices do
   @spec invoices_to_generate(pos_integer()) :: pos_integer()
   def invoices_to_generate(id) do
     rec_inv = get!(id)
-    today =  to_date(utc_now)
-    max_date = Date.add(starting_date, )
-    if Date.compare(finishing_date, Date.add())
-    number_using_date = number_of_inv_using_date(today, rec_inv.period, rec_inv.period_type, rec_inv.finishing_date)
-    number_using_max = number_of_inv(rec_inv.starting_date, rec_inv.period, rec_inv.period_type, rec_inv.max_ocurrences)
+    today = to_date(utc_now())
+
+    max_date =
+      cond do
+        is_nil(rec_inv.max_ocurrences) && is_nil(rec_inv.finishing_date) ->
+          [today, today]
+
+        is_nil(rec_inv.max_ocurrences) ->
+          [Date.add(rec_inv.finishing_date, 1), rec_inv.finishing_date]
+
+        true ->
+          date_max_ocurrences =
+            date_from_max_ocurrences(
+              rec_inv.starting_date,
+              rec_inv.period,
+              rec_inv.period_type,
+              rec_inv.max_ocurrences
+            )
+
+          finishing_date =
+            if is_nil(rec_inv.finishing_date),
+              do: Date.add(date_max_ocurrences, 1),
+              else: rec_inv.finishing_date
+
+          [date_max_ocurrences, finishing_date]
+      end
+      |> Enum.sort(Date)
+      |> List.first()
+
+    number_of_inv(today, rec_inv.period, rec_inv.period_type, max_date)
   end
 
-  #Right now returns the number of invoices to generate counting today but not the finishing_date
-  defp number_of_inv(today, period, "Daily", %Date{} = max_date) do
-    Stream.iterate(today, &( Date.add( &1, period) ))
-    |> Enum.take_while( &Date.compare(&1, finishing_date) != :eq)
+  # Right now returns the number of invoices to generate counting today but and the finishing_date
+  @spec number_of_inv(Date.t(), pos_integer(), binary, Date.t()) :: pos_integer()
+  defp number_of_inv(%Date{} = today, period, period_type, %Date{} = max_date) do
+    Stream.iterate(today, &Date.add(&1, days_to_sum_for_next(&1, period, period_type)))
+    |> Enum.take_while(&(Date.compare(&1, max_date) != :gt))
     |> length()
   end
 
-  defp number_of_inv(today, period, "Monthly", %Date{} = max_date) do
-    Stream.iterate(today, &( Date.add(&1, days_to_sum(&1, period)) ))
+  # Returns the theoretical date of max_ocurrences ending counting that first invoice is on starting_date
+  @spec date_from_max_ocurrences(Date.t(), pos_integer(), binary, pos_integer()) :: Date.t()
+  defp date_from_max_ocurrences(%Date{} = starting_date, _period, _period_type, 1),
+    do: starting_date
+
+  defp date_from_max_ocurrences(%Date{} = starting_date, period, period_type, max_ocurrences) do
+    next_date = Date.add(starting_date, days_to_sum_for_next(starting_date, 1, period_type))
+    date_from_max_ocurrences(next_date, period, period_type, max_ocurrences - 1)
   end
 
+  @spec days_to_sum_for_next(Date.t(), pos_integer(), binary) :: pos_integer
+  defp days_to_sum_for_next(_date, 0, _period_type), do: 0
+  defp days_to_sum_for_next(_date, period, "Daily"), do: period
+
+  defp days_to_sum_for_next(date, period, "Monthly") do
+    days_this_month = :calendar.last_day_of_the_month(date.year, date.month)
+    days_this_month + days_to_sum_for_next(Date.add(date, days_this_month), period - 1, "Monthly")
+  end
+
+  defp days_to_sum_for_next(date, period, "Yearly") do
+    days_this_year = if Date.leap_year?(date), do: 366, else: 365
+    days_this_year + days_to_sum_for_next(Date.add(date, days_this_year), period - 1, "Yearly")
+  end
 end
