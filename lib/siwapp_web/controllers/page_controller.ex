80fcc017f887dc3b8e9662a5e34f63fc347d7a60
Pabloj20 <pablo.rubio@doofinder.com>
2022-01-19 10:38:37 +0100

    mix format

--- lib/siwapp_web/controllers/page_controller.ex
+++ lib/siwapp_web/controllers/page_controller.ex
@@ -1,6 +1,7 @@
 defmodule SiwappWeb.PageController do
   use SiwappWeb, :controller
   alias Siwapp.Invoices
+
   def show_invoice(conn, %{"id" => id}) do
     invoice = Invoices.get!(String.to_integer(id))
 
