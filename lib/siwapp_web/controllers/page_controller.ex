5cb09b5e0de3207867bf5f2807402d0eb6dfb5a3
Pabloj20 <pablo.rubio@doofinder.com>
2022-01-19 11:24:45 +0100

    Not using a pipeline in page_controller.ex

--- lib/siwapp_web/controllers/page_controller.ex
+++ lib/siwapp_web/controllers/page_controller.ex
@@ -4,11 +4,7 @@ defmodule SiwappWeb.PageController do
 
   def show_invoice(conn, %{"id" => id}) do
     invoice = Invoices.get!(String.to_integer(id))
-
-    conn =
-      conn
-      |> assign(:invoice, invoice)
-
+    conn = assign(conn, :invoice, invoice)
     render(conn, "show_invoice.html")
   end
 end
