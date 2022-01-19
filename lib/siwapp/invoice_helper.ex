7281ea7bce3a4532e97de779f32465b0ee461a95
Sergio Olivares <sergio.olivares@doofinder.com>
2022-01-19 14:15:09 +0100

    It's working

--- lib/siwapp/invoice_helper.ex
+++ lib/siwapp/invoice_helper.ex
@@ -15,35 +15,42 @@ defmodule Siwapp.InvoiceHelper do
 
       case Customers.get(identification, name) do
         nil ->
-          customer = Customer.changeset(%Customer{}, changeset.changes)
+          customer_changeset = Customer.changeset(%Customer{}, changeset.changes)
+          changeset = put_assoc(changeset, :customer, customer_changeset)
+
           changeset
-          |> put_assoc(:customer, customer)
-          |> traverse_customer_errors()
+          |> traverse_errors(& &1)
+          |> Map.get(:customer, [])
+          |> bring_customer_errors(changeset)
 
         customer ->
           put_change(changeset, :customer_id, customer.id)
       end
     else
-      customer_changeset = Customer.changeset(%Customer{}, changeset.changes)
-      changeset = bring_customer_errors(customer_changeset.errors, changeset)
+      customer_changeset = get_customer_changeset(changeset)
+      errors = Enum.map(customer_changeset.errors, fn {key, {message, opts}} -> {key, [{message, opts}]} end)
 
-      IO.inspect changeset
+      bring_customer_errors(errors, changeset)
     end
   end
 
-  defp traverse_customer_errors(changeset) do
-    traverse_errors(changeset, & &1)
-    |> Map.get(:customer, [])
-    |> Enum.map(fn {key, [value]} -> {key, value} end)
-    |> bring_customer_errors(changeset)
+  defp get_customer_changeset(changeset) do
+    customer_data =
+      Map.take(changeset.data, [
+        :name,
+        :identification,
+        :email,
+        :contact_person,
+        :invoicing_address,
+        :shipping_address
+      ])
+
+      Customer.changeset(struct(Customer, customer_data), changeset.changes)
   end
 
   defp bring_customer_errors(errors, changeset) do
-    errors
-    |> Enum.reduce(changeset, fn error, changeset -> add_customer_error(changeset, error) end)
+    Enum.reduce(errors, changeset, fn error, new_changeset -> add_customer_error(new_changeset, error) end)
   end
 
-  defp add_customer_error(changeset, {key, {message, opts}}) do
-    add_error(changeset, key, message, opts)
-  end
+  defp add_customer_error(changeset, {key, [{message, opts}]}), do: add_error(changeset, key, message, opts)
 end
