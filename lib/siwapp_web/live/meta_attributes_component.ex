2dd9c5b2d84ef44ba718c63b2b4e6b4f9779eb0a
peillis <enrique@doofinder.com>
2022-01-24 19:24:56 +0100

    wip

--- lib/siwapp_web/live/meta_attributes_component.ex
+++ lib/siwapp_web/live/meta_attributes_component.ex
@@ -3,58 +3,51 @@ defmodule SiwappWeb.MetaAttributesComponent do
 
   use SiwappWeb, :live_component
 
-  def mount(socket) do
-    {:ok, init_socket(socket)}
+  alias Phoenix.HTML.Form
+
+  def update(assigns, socket) do
+    attributes =
+      case Form.input_value(assigns.f, :meta_attributes) do
+        "" -> %{}
+        attrs -> attrs
+      end
+
+    socket =
+      socket
+      |> assign(new_key: "")
+      |> assign(new_value: "")
+      |> assign(attributes: attributes)
+
+    {:ok, socket}
   end
 
   def render(assigns) do
     ~H"""
     <fieldset>
       <h2>Meta Attributes</h2>
-      <%= for {k, v} <- @meta_attributes do %>
+      <%= for {k, v} <- @attributes do %>
         <div class="field is-horizontal field-body">
-          <input class="input field" type="text" name="meta[keys][]" value={k} />
-          <input class="input field" type="text" name="meta[values][]" value={v} />
+          <input class="input field" type="text" name="meta[values][]" value={k} />
+          <input class="input field" type="text" name={"customer[meta_attributes][#{k}]"} value={v} />
           <a class="button is-danger field" phx-click="remove" phx-value-key={k} phx-target={@myself}>Remove</a>
-          <br/>
         </div>
       <% end %>
+
+      <%= if @attributes == %{} do %>
+        <input type="hidden" name="customer[meta_attributes]" />
+      <% end %>
+
       <div class="field is-horizontal field-body">
         <input class="input field" type="text" name="meta[keys][]" phx-blur="changing-key" phx-target={@myself} placeholder="Key"/>
         <input class="input field" type="text" name="meta[values][]" phx-blur="changing-value" phx-target={@myself} placeholder="Value"/>
         <a class="button is-success field" phx-click="add" phx-target={@myself}>Add</a>
-        <br/>
       </div>
     </fieldset>
     """
   end
 
-  @doc """
-  Merge the `params` received from the form with the meta_attributes.
-
-  ## Examples
-
-      iex> merge(
-      ...>   %{"what" => "ever"},
-      ...>   %{"keys" => ["mykey"], "values" => ["myvalue"]}
-      ...>)
-      %{"what" => "ever", "meta_attributes" => %{"mykey" => "myvalue"}}
-  """
-  @spec merge(map, map) :: map
-  def merge(params, meta) do
-    meta_attributes =
-      Enum.zip(meta["keys"], meta["values"])
-      |> Map.new()
-      |> Map.delete("")
-
-    Map.put(params, "meta_attributes", meta_attributes)
-  end
-
   def handle_event("remove", %{"key" => key}, socket) do
-    new = Map.delete(socket.assigns.meta_attributes, key)
-    send_update(__MODULE__, id: "meta_attributes", meta_attributes: new)
-
-    {:noreply, socket}
+    {:noreply, update(socket, :attributes, &Map.delete(&1, key))}
   end
 
   def handle_event("changing-key", %{"value" => value}, socket) do
@@ -66,27 +59,8 @@ defmodule SiwappWeb.MetaAttributesComponent do
   end
 
   def handle_event("add", _params, %{assigns: assigns} = socket) do
-    new =
-      case assigns.new_key do
-        "" ->
-          assigns.meta_attributes
+    socket = update(socket, :attributes, &Map.put(&1, assigns.new_key, assigns.new_value))
 
-        _ ->
-          Map.put(
-            assigns.meta_attributes,
-            assigns.new_key,
-            assigns.new_value
-          )
-      end
-
-    send_update(__MODULE__, id: "meta_attributes", meta_attributes: new)
-
-    {:noreply, init_socket(socket)}
-  end
-
-  defp init_socket(socket) do
-    socket
-    |> assign(:new_key, "")
-    |> assign(:new_value, "")
+    {:noreply, socket}
   end
 end
