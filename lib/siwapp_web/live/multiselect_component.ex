1de5f78f2947c28919a39805532ec8c59982c65c
Sergio Olivares <sergio.olivares@doofinder.com>
2022-01-25 13:13:46 +0100

    Removed javascript solution

--- lib/siwapp_web/live/multiselect_component.ex
+++ lib/siwapp_web/live/multiselect_component.ex
@@ -6,11 +6,13 @@ defmodule SiwappWeb.MultiselectComponent do
   alias Phoenix.LiveView.JS
 
   def update(assigns, socket) do
+    "ms-" <> index = assigns.id
+
     socket =
       socket
       |> assign(selected: MapSet.new(assigns.selected))
       |> assign(name: assigns.name)
-      |> assign(id: assigns.id)
+      |> assign(index: index)
       |> assign(options: MapSet.new(assigns.options))
 
     {:ok, socket}
@@ -20,38 +22,39 @@ defmodule SiwappWeb.MultiselectComponent do
     ~H"""
     <div class="control msa-wrapper">
       <%= for {k, _v} <- @selected do %>
-        <input type="hidden" id={"invoice_items_#{@id}_taxes"} name={"#{@name}[]"} value={k}>
+        <input type="hidden" name={"#{@name}[]"} value={k}>
       <% end %>
-      <div class="input input-presentation" phx-click={JS.toggle(to: "#tag-list-#{@id}")}>
+      <div class="input input-presentation" phx-click={JS.toggle(to: "#tag-list-#{@index}")}>
         <span class="placeholder"></span>
         <%= for {k, v} <- @selected do %>
           <div class="tag-badge">
             <span><%= k %></span>
-            <button type="button" phx-click={JS.push("remove", target: @myself, value: %{id: @id, key: k, val: v})}>x</button>
+            <button type="button" phx-click={JS.push("remove", target: @myself, value: %{index: @index, key: k, val: v})}>x</button>
           </div>
         <% end %>
       </div>
-      <ul id={"tag-list-#{@id}"} class="tag-list" style="display: none;">
+      <ul id={"tag-list-#{@index}"} class="tag-list" style="display: none;">
         <%= for {k, v} <- not_selected(@options, @selected) do %>
-          <li phx-click={JS.push("add", target: @myself, value: %{id: @id, key: k, val: v}) |> JS.toggle(to: "#tag-list-#{@id}")}><%= k %></li>
+          <li phx-click={JS.push("add", target: @myself, value: %{index: @index, key: k, val: v}) |> JS.toggle(to: "#tag-list-#{@index}")}><%= k %></li>
         <% end %>
       </ul>
     </div>
     """
   end
 
-  def handle_event("remove", %{"id" => id, "key" => key, "val" => value}, socket) do
-    {:noreply,
-     socket
-     |> Phoenix.LiveView.push_event("force-validate", %{id: "invoice_items_#{id}_taxes"})
-     |> update(:selected, &MapSet.delete(&1, {key, value}))}
+  def handle_event("remove", %{"index" => index, "key" => key, "val" => value}, socket) do
+    selected = MapSet.delete(socket.assigns.selected, {key, value})
+    send self(), {:taxes_updated, %{index: String.to_integer(index), selected: Enum.map(selected, fn {k, _v} -> k end)}}
+
+    {:noreply, assign(socket, :selected, selected)}
   end
 
-  def handle_event("add", %{"id" => id, "key" => key, "val" => value}, socket) do
-    {:noreply,
-     socket
-     |> Phoenix.LiveView.push_event("force-validate", %{id: "invoice_items_" <> id <> "_taxes"})
-     |> update(:selected, &MapSet.put(&1, {key, value}))}
+  def handle_event("add", %{"index" => index, "key" => key, "val" => value}, socket) do
+    selected = MapSet.put(socket.assigns.selected, {key, value})
+
+    send self(), {:taxes_updated, %{index: String.to_integer(index), selected: Enum.map(selected, fn {k, _v} -> k end)}}
+
+    {:noreply, assign(socket, :selected, selected)}
   end
 
   defp not_selected(options, selected) do
