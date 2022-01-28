259f9833c6e942e261bcf0afd5cf13e5c9e8b867
Pabloj20 <pablo.rubio@doofinder.com>
2022-01-28 11:18:58 +0100

    Downloading pdf by the browser and the api

--- lib/siwapp_web/controllers/iframe_controller.ex
+++ lib/siwapp_web/controllers/iframe_controller.ex
@@ -8,32 +8,10 @@ defmodule SiwappWeb.IframeController do
 
   def iframe(conn, %{"id" => id}) do
     invoice = Invoices.get!(String.to_integer(id), preload: [{:items, :taxes}, :series])
-    template = Templates.get(:print_default).template
+    str_template = Templates.string_template(invoice)
 
-    invoice_eval_data =
-      invoice
-      |> Map.from_struct()
-      |> Enum.map(fn {key, value} -> {key, value} end)
-
-    all_eval_data =
-      invoice_eval_data ++
-        [have_discount?: have_items_discount?(invoice.items), status: Invoices.status(invoice)]
-
-    str_template = EEx.eval_string(template, all_eval_data)
     html(conn, str_template)
   end
 
-  defp have_items_discount?([]) do
-    false
-  end
 
-  defp have_items_discount?(items) do
-    [h | t] = items
-
-    if h.discount != 0 do
-      true
-    else
-      have_items_discount?(t)
-    end
-  end
 end
