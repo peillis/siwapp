6d0c48e97d93316135981c280171bcceebc38b1d
nurietzche <nuria.gallego@doofinder.com>
2022-01-28 21:47:13 +0100

    Seeds de recurring_invoice correctas ahora

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -150,7 +150,7 @@ recurring_invoices = [
         "quantity" => 1,
         "unitary_cost" => 0,
         "discount" => 0,
-        "taxes" => "RETENTION"
+        "taxes" => ["RETENTION"]
       }
     ]
   },
