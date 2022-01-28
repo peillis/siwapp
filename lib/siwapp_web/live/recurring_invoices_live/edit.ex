5c049a80e16f32f055dffbfae66f8d2bdb817089
nurietzche <nuria.gallego@doofinder.com>
2022-01-28 18:43:35 +0100

    LiveView para recurring_invoice lista Los items son una lista de mapas y almacenan sus taxes

--- lib/siwapp_web/live/recurring_invoices_live/edit.ex
+++ lib/siwapp_web/live/recurring_invoices_live/edit.ex
@@ -3,6 +3,7 @@ defmodule SiwappWeb.RecurringInvoicesLive.Edit do
   use SiwappWeb, :live_view
 
   alias Siwapp.Commons
+  alias Siwapp.Invoices.Item
   alias Siwapp.RecurringInvoices
   alias Siwapp.RecurringInvoices.RecurringInvoice
 
@@ -11,6 +12,7 @@ defmodule SiwappWeb.RecurringInvoicesLive.Edit do
      socket
      |> assign(:series, Commons.list_series())
      |> assign(:customer_suggestions, [])}
+    }
   end
 
   def handle_params(params, _url, socket) do
@@ -18,16 +20,16 @@ defmodule SiwappWeb.RecurringInvoicesLive.Edit do
   end
 
   def apply_action(socket, :new, _params) do
-    new_recurring_invoice = %RecurringInvoice{items: [%{}]}
+    new_recurring_invoice = %RecurringInvoice{}
 
     socket
     |> assign(:action, :new)
     |> assign(:page_title, "New Recurring Invoice")
     |> assign(:recurring_invoice, new_recurring_invoice)
+    |> assign(:items, [new_item()])
     |> assign(:changeset, RecurringInvoices.change(new_recurring_invoice))
     |> assign(:customer_name, "")
   end
-
   def apply_action(socket, :edit, %{"id" => id}) do
     recurring_invoice = RecurringInvoices.get!(String.to_integer(id), :preload)
 
@@ -35,11 +37,13 @@ defmodule SiwappWeb.RecurringInvoicesLive.Edit do
     |> assign(:action, :edit)
     |> assign(:page_title, recurring_invoice.name)
     |> assign(:recurring_invoice, recurring_invoice)
+    |> assign(:items, recurring_invoice.items || [])
     |> assign(:changeset, RecurringInvoices.change(recurring_invoice))
     |> assign(:customer_name, recurring_invoice.name)
   end
 
-  def handle_event("save", %{"recurring_invoice" => params}, socket) do
+  def handle_event("save", %{"recurring_invoice" => rec_params} = params, socket) do
+    {items, params} = build_params(params, rec_params)
     result =
       case socket.assigns.live_action do
         :new -> RecurringInvoices.create(params)
@@ -60,12 +64,20 @@ defmodule SiwappWeb.RecurringInvoicesLive.Edit do
     end
   end
 
-  def handle_event("validate", %{"recurring_invoice" => params}, socket) do
-    changeset =
-      socket.assigns.recurring_invoice
-      |> RecurringInvoices.change(params)
+  def handle_event("validate", %{ "recurring_invoice" => rec_params } = params, socket) do
+    {items, params} = build_params(params, rec_params)
+    changeset = RecurringInvoices.change(socket.assigns.recurring_invoice, params)
+    socket =
+      socket
+      |> assign(:changeset, changeset)
+      |> assign(:items, items)
+    {:noreply, socket}
+  end
 
-    {:noreply, assign(socket, :changeset, changeset)}
+  def handle_event("add_item", _, socket) do
+    items =
+      socket.assigns.items ++ [new_item()]
+    {:noreply, assign(socket, :items, items)}
   end
 
   def handle_info({:update_changeset, params}, socket) do
@@ -75,4 +87,25 @@ defmodule SiwappWeb.RecurringInvoicesLive.Edit do
 
     {:noreply, assign(socket, :changeset, changeset)}
   end
+  def handle_event("remove_item", %{"item-id" => item_id}, socket) do
+    items = List.delete_at(socket.assigns.items, String.to_integer(item_id))
+    {:noreply, assign(socket, items: items)}
+  end
+
+  defp build_params(params, rec_params) do
+    taxes_params = get_taxes_params(params)
+    items =
+      params
+      |> get_items_params()
+      |> merge_taxes_with_item(taxes_params)
+      |> Enum.map(fn {index, item}-> item end)
+    params = Map.put(rec_params,"items", items)
+    {items, params}
+  end
+
+  defp new_item, do: %{"description" => "", "unitary_cost" => 0, "quantity" => 1, "discount" => 0, "taxes" => []}
+  defp get_items_params(params), do: params["items"] || %{}
+  defp get_taxes_params(params), do: params["invoice"]["items"] || %{}
+  defp merge_taxes_with_item(items_params, taxes_params), do: Map.new(items_params, fn {index, item} -> {index, Map.merge(item, (taxes_params[index] || %{}) )} end)
+
 end
