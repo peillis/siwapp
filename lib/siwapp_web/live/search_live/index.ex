d77f5601a351f0206fc786aae0cf5db281a603e9
Pabloj20 <pablo.rubio@doofinder.com>
2022-01-31 12:19:44 +0100

    Search form

---
+++ lib/siwapp_web/live/search_live/index.ex
@@ -0,0 +1,13 @@
+defmodule SiwappWeb.SearchLive.Index do
+  use Phoenix.LiveView, layout: {SiwappWeb.LayoutView, "search_live.html"}
+  alias Phoenix.LiveView.JS
+
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  def toogle() do
+    JS.toggle(to: "#search-menu")
+  end
+
+end
