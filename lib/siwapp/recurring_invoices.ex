cbcf5bb728c528ea0757f7498ce81c3ce7504b5e
nurietzche <nuria.gallego@doofinder.com>
2022-01-31 12:50:04 +0100

    Generate invoices lista! Simplemente faltará enviar por email una factura cuando el campo send_by_email sea cierto

--- lib/siwapp/recurring_invoices.ex
+++ lib/siwapp/recurring_invoices.ex
@@ -4,7 +4,7 @@ defmodule Siwapp.RecurringInvoices do
   """
   import Ecto.Query, warn: false
 
-  alias Siwapp.Invoices.{Invoice, InvoiceQuery}
+  alias Siwapp.Invoices.{Invoice, InvoiceQuery, Item}
   alias Siwapp.Query
   alias Siwapp.RecurringInvoices.RecurringInvoice
   alias Siwapp.Repo
@@ -35,6 +35,11 @@ defmodule Siwapp.RecurringInvoices do
     |> Repo.insert()
   end
 
+  @spec change(RecurringInvoice.t(), map) :: Ecto.Changeset.t()
+  def change(%RecurringInvoice{} = recurring_invoice, attrs \\ %{}) do
+    RecurringInvoice.changeset(recurring_invoice, attrs)
+  end
+
   @spec update(RecurringInvoice.t(), map) ::
           {:ok, RecurringInvoice.t()} | {:error, Ecto.Changeset.t()}
   def update(recurring_invoice, attrs) do
@@ -49,33 +54,64 @@ defmodule Siwapp.RecurringInvoices do
     Repo.delete(recurring_invoice)
   end
 
+  @doc """
+  Generates invoices associated to recurring_invoice if this is enabled
+  """
   def generate_invoices(id) do
     rec_inv = Repo.get!(RecurringInvoice, id)
     invoices_to_do = invoices_to_generate(id)
-    Enum.each(1..invoices_to_do, &build_invoice(rec_inv))
+    for _i <- 1..invoices_to_do, do: if rec_inv.enabled, do: Repo.insert(build_invoice(rec_inv))
   end
 
-  defp build_invoice(recurring_invoice) do
-    common_values =
-      recurring_invoice
+  # Builds Invoice struct from recurring_invoice
+  @spec build_invoice(RecurringInvoice.t()) :: Invoice.t()
+  defp build_invoice(rec_inv) do
+    common_parameters =
+      rec_inv
       |> Map.from_struct()
       |> Map.to_list()
       |> Keyword.filter(fn {key, _value} -> Enum.member?(identical_fields(), key) end)
+    keywords_invoice =
+      common_parameters
+      |> Keyword.put_new(:recurring_invoice_id, rec_inv.id)
+      |> maybe_add_due_date(rec_inv.days_to_due)
+      |> insert_items(rec_inv.items)
+    struct(Invoice, keywords_invoice)
   end
 
-  defp identical_fields(), do: [:name, :identification, :email, :contact_person, :invoicing_address, :shipping_address, :net_amount, :gross_amount, :notes, :terms, :meta_attributes, :customer_id, :series_id, :currency]
-
-  @spec change(RecurringInvoice.t(), map) :: Ecto.Changeset.t()
-  def change(%RecurringInvoice{} = recurring_invoice, attrs \\ %{}) do
-    RecurringInvoice.changeset(recurring_invoice, attrs)
+  @spec maybe_add_due_date(Keyword.t(), integer) :: Keyword.t()
+  defp maybe_add_due_date(keywords, days_to_due) do
+    if days_to_due do
+      due_date = Date.add(Date.utc_today(), days_to_due)
+      Keyword.put_new(keywords, :due_date, due_date)
+    else
+      keywords
+    end
   end
 
-  @doc """
-  Given a recurring_invoice id, returns the amount of invoices that should
-  be generated
-  """
+  @spec insert_items(Keyword.t()) :: Keyword.t()
+  defp insert_items(keywords, []), do: keywords
+  defp insert_items(keywords, list), do: keywords ++ [{:items, build_items(list)}]
+
+  @spec build_items(list) :: [] | [Item.t()]
+  defp build_items([]), do: []
+  defp build_items([ h | t]) do
+    item_parameters = translate_item_fields(h)
+    [struct(Item, item_parameters)] ++ build_items(t)
+  end
+
+  @spec translate_item_fields(map) :: Keyword.t()
+  defp translate_item_fields(%{"description" => d, "discount" => di, "quantity" => q, "taxes" => t, "unitary_cost" => u}) do
+    taxes = Enum.map(t, &(Siwapp.Commons.get_tax_by_name(&1)))
+  [{:description, d}, {:unitary_cost, String.to_integer(u)}, {:quantity, String.to_integer(q)}, {:discount, String.to_integer(di)}, {:taxes, taxes}]
+  end
+
+  @spec identical_fields :: Keyword.t()
+  defp identical_fields, do: [:name, :identification, :email, :contact_person, :invoicing_address, :shipping_address, :net_amount, :gross_amount, :notes, :terms, :meta_attributes, :customer_id, :series_id, :currency]
+
+  #Given a recurring_invoice id, returns the amount of invoices that should  be generated
   @spec invoices_to_generate(pos_integer()) :: integer
-  def invoices_to_generate(id) do
+  defp invoices_to_generate(id) do
     theoretical_number_of_inv_generated(id) - generated_invoices(id)
   end
 
