4da28230c515cf4cca009e11e4577fec793cca6d
olirods <sergio.olivares@doofinder.com>
2022-01-31 13:28:13 +0100

    new button also for static pages

--- lib/siwapp_web/views/layout_view.ex
+++ lib/siwapp_web/views/layout_view.ex
@@ -5,29 +5,29 @@ defmodule SiwappWeb.LayoutView do
   # so we instruct Elixir to not warn if the dashboard route is missing.
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
 
-  def shared_button(conn) do
-    case conn.view do
+  def shared_button(socket) do
+    case socket.view do
       n when n in [SiwappWeb.SeriesLive.Index, SiwappWeb.SeriesLive.FormComponent] ->
-        new_button("New Series", Routes.series_index_path(conn, :new))
+        new_button("New Series", Routes.series_index_path(socket, :new))
 
       n when n in [SiwappWeb.CustomerLive.Index, SiwappWeb.CustomerLive.Edit] ->
-        new_button("New Customer", Routes.customer_edit_path(conn, :new))
+        new_button("New Customer", Routes.customer_edit_path(socket, :new))
 
       n when n in [SiwappWeb.TaxesLive.Index, SiwappWeb.TaxesLive.FormComponent] ->
-        new_button("New Tax", Routes.taxes_index_path(conn, :new))
+        new_button("New Tax", Routes.taxes_index_path(socket, :new))
 
       n when n in [SiwappWeb.TemplatesLive.Index, SiwappWeb.TemplatesLive.Edit] ->
-        new_button("New Template", Routes.templates_edit_path(conn, :new))
+        new_button("New Template", Routes.templates_edit_path(socket, :new))
 
       n when n in [SiwappWeb.RecurringInvoicesLive.Index, SiwappWeb.RecurringInvoicesLive.Edit] ->
-        new_button("New Recurring Invoice", Routes.recurring_invoices_edit_path(conn, :new))
+        new_button("New Recurring Invoice", Routes.recurring_invoices_edit_path(socket, :new))
 
       _ ->
-        new_button("New Invoice", Routes.invoices_edit_path(conn, :new))
+        new_button("New Invoice", Routes.invoices_edit_path(socket, :new))
     end
   end
 
   defp new_button(text, to) do
-    button(text, to: to, method: :get, class: "button is-primary")
+    live_redirect(text, to: to, class: "button is-primary")
   end
 end
