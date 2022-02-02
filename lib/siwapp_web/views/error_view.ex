a31da8126ff59460d5db4c49861f0abe5432c0f1
peillis <enrique@doofinder.com>
2022-02-02 13:17:36 +0100

    fix for dialyzer

--- lib/siwapp_web/views/error_view.ex
+++ lib/siwapp_web/views/error_view.ex
@@ -10,7 +10,4 @@ defmodule SiwappWeb.ErrorView do
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.html" becomes
   # "Not Found".
-  def template_not_found(template, _assigns) do
-    Phoenix.Controller.status_message_from_template(template)
-  end
 end
