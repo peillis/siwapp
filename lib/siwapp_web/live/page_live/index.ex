d365fab6dcc8851158ffd3218ead66a92b325cd0
Sergio Olivares <sergio.olivares@doofinder.com>
2022-02-03 14:15:21 +0100

    scroll added to home page again

--- lib/siwapp_web/live/page_live/index.ex
+++ lib/siwapp_web/live/page_live/index.ex
@@ -6,6 +6,13 @@ defmodule SiwappWeb.PageLive.Index do
   alias Siwapp.Invoices
   alias SiwappWeb.GraphicHelpers
 
+  def mount(_params, _session, socket) do
+    {:ok,
+     socket
+     |> assign(:page, 0)
+     |> assign(:invoices, Invoices.list_past_due(0))}
+  end
+
   @doc """
   Creates the dashboard chart with information about invoices (amounts of money per day).
   """
@@ -14,6 +21,21 @@ defmodule SiwappWeb.PageLive.Index do
     Invoices.Statistics.get_data()
     |> Enum.map(&date_to_naive_type/1)
     |> GraphicHelpers.line_plot()
+  end
+
+  def handle_event("load-more", _, socket) do
+    %{
+      page: page,
+      invoices: invoices
+    } = socket.assigns
+
+    {
+      :noreply,
+      assign(socket,
+        invoices: invoices ++ Invoices.list_past_due(page + 1),
+        page: page + 1
+      )
+    }
   end
 
   # This is necessary because Contex.DateScale only accepts DateTime or NaiveDateTime types.
