314b54003d05338f6a78a71798fd46168a41e783
Rodrigo Molina <rodrigomolina@doofinder.com>
2022-02-04 10:10:38 +0100

    pequeños cambios en los test

--- test/support/fixtures/commons_fixtures.ex
+++ test/support/fixtures/commons_fixtures.ex
@@ -10,6 +10,10 @@ defmodule Siwapp.CommonsFixtures do
 
   def unique_series_code, do: "#{System.unique_integer()}"
 
+  def unique_taxes_name, do: "#{System.unique_integer()}"
+
+  def unique_taxes_value, do: :rand.uniform(30)
+
   def series_fixture(attrs \\ %{}) do
     {:ok, series} =
       attrs
@@ -24,6 +28,24 @@ defmodule Siwapp.CommonsFixtures do
       name: unique_series_name(),
       code: unique_series_code(),
       first_number: 1
+    })
+  end
+
+  def taxes_fixture(attrs \\ %{}) do
+    {:ok, tax} =
+      attrs
+      |> valid_taxes_attributes()
+      |> Commons.create_tax()
+
+    Cachex.clear(:my_cache)
+
+    tax
+  end
+
+  def valid_taxes_attributes(attrs \\ %{}) do
+    Enum.into(attrs, %{
+      name: unique_taxes_name(),
+      value: unique_taxes_value()
     })
   end
 end
