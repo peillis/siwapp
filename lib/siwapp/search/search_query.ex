a5fad794acd4650493ca924c32c7c6e7ca4c0cec
Pabloj20 <pablo.rubio@doofinder.com>
2022-02-09 14:15:29 +0100

    Final version

--- lib/siwapp/search/search_query.ex
+++ lib/siwapp/search/search_query.ex
@@ -129,4 +129,78 @@ defmodule Siwapp.Search.SearchQuery do
     |> String.downcase()
     |> String.to_atom()
   end
+
+  defp from_or_to(query, key, value) do
+    if String.contains?(key, "_from_") do
+      type_of_from_date(query, key, value)
+    else
+      type_of_to_date(query, key, value)
+    end
+  end
+
+  defp type_of_from_date(query, key, value) do
+    cond do
+      String.starts_with?(key, "issue") ->
+        InvoiceQuery.issue_date_gteq(query, value)
+
+      String.starts_with?(key, "starting") ->
+        RecurringInvoiceQuery.starting_date_gteq(query, value)
+
+      true ->
+        RecurringInvoiceQuery.finishing_date_gteq(query, value)
+    end
+  end
+
+  defp type_of_to_date(query, key, value) do
+    cond do
+      String.starts_with?(key, "issue") ->
+        InvoiceQuery.issue_date_lteq(query, value)
+
+      String.starts_with?(key, "starting") ->
+        RecurringInvoiceQuery.starting_date_lteq(query, value)
+
+      true ->
+        RecurringInvoiceQuery.finishing_date_lteq(query, value)
+    end
+  end
+
+  defp type_of_status(query, value) do
+    case value do
+      v when v in ["Draft", "Paid", "Failed"] ->
+        value = convert_to_atom(value)
+
+        query
+        |> where([q], field(q, ^value) == true)
+
+      "Pending" ->
+        query
+        |> where(draft: false)
+        |> where(paid: false)
+        |> where(failed: false)
+        |> where([q], is_nil(q.due_date) or (q.due_date > ^Date.utc_today()))
+
+      "Past Due" ->
+        query
+        |> where(draft: false)
+        |> where(paid: false)
+        |> where(failed: false)
+        |> where([q], not is_nil(q.due_date))
+        |> where([q], q.due_date < ^Date.utc_today())
+    end
+  end
+
+  defp convert_to_atom(value) do
+    value
+    |> String.downcase()
+    |> String.to_atom()
+  end
+
+  defp get_all_keys(query) do
+    query
+    |> select([q], q.meta_attributes)
+    |> Repo.all()
+    |> Enum.map(&Map.keys(&1))
+    |> List.flatten()
+    |> Enum.uniq()
+  end
 end
