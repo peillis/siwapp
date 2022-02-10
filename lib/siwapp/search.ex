8bc082c4b1a8a47e439f0918e7a44ee4140641b5
Pabloj20 <pablo.rubio@doofinder.com>
2022-02-10 09:51:30 +0100

    2 arguments instead of 1 in filters function

--- lib/siwapp/search.ex
+++ lib/siwapp/search.ex
@@ -6,6 +6,7 @@ defmodule Siwapp.Search do
   alias Siwapp.{Query, Repo}
   alias Siwapp.Search.SearchQuery
 
+<<<<<<< HEAD
   @doc """
   Filter invoices, customers or recurring_invoices by the selected parameters
   """
@@ -20,5 +21,22 @@ defmodule Siwapp.Search do
     |> SearchQuery.name_email_or_id(value)
     |> Query.list_preload(:series)
     |> Repo.all()
+=======
+  def filters(query, value) do
+    query
+    |> SearchQuery.name_email_or_id(value)
+    |> maybe_preload()
+    |> Repo.all()
+  end
+
+  def maybe_preload(query) do
+    {view, _} = query.from.source
+
+    if view in ["invoices", "recurring_invoices"] do
+      Query.list_preload(query, :series)
+    else
+      query
+    end
+>>>>>>> dbd416d (2 arguments instead of 1 in filters function)
   end
 end
