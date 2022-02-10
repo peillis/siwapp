42f8f5bb6872d492336e160e47ef2662e038cbc4
Pabloj20 <pablo.rubio@doofinder.com>
2022-02-10 13:01:12 +0100

    Docs and pattern matching

--- lib/siwapp/search.ex
+++ lib/siwapp/search.ex
@@ -4,9 +4,9 @@ defmodule Siwapp.Search do
   Search Context
   """
   alias Siwapp.{Query, Repo}
+  alias Siwapp.Customers.Customer
   alias Siwapp.Search.SearchQuery
 
-<<<<<<< HEAD
   @doc """
   Filter invoices, customers or recurring_invoices by the selected parameters
   """
@@ -21,22 +21,5 @@ defmodule Siwapp.Search do
     |> SearchQuery.name_email_or_id(value)
     |> Query.list_preload(:series)
     |> Repo.all()
-=======
-  def filters(query, value) do
-    query
-    |> SearchQuery.name_email_or_id(value)
-    |> maybe_preload()
-    |> Repo.all()
-  end
-
-  def maybe_preload(query) do
-    {view, _} = query.from.source
-
-    if view in ["invoices", "recurring_invoices"] do
-      Query.list_preload(query, :series)
-    else
-      query
-    end
->>>>>>> dbd416d (2 arguments instead of 1 in filters function)
   end
 end
