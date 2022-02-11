68f12cf82607e8a2d25851c7bc236b7102ba0366
Pabloj20 <pablo.rubio@doofinder.com>
2022-02-11 13:43:55 +0100

    Update value query case

--- lib/siwapp/search.ex
+++ lib/siwapp/search.ex
@@ -11,15 +11,19 @@ defmodule Siwapp.Search do
   Filter invoices, customers or recurring_invoices by the selected parameters
   """
   @spec filters(Ecto.Queryable.t(), binary) :: list
-  def filters(Siwapp.Customers.Customer = customer, value) do
-    customer
-    |> SearchQuery.name_email_or_id(value)
+  def filters(Siwapp.Customers.Customer = customer, values) do
+    values
+    |> Enum.reduce(customer, fn {key, value}, acc_query ->
+      SearchQuery.filter_by(acc_query, key, value)
+    end)
     |> Repo.all()
   end
 
-  def filters(query, value) do
-    query
-    |> SearchQuery.name_email_or_id(value)
+  def filters(query, values) do
+    values
+    |> Enum.reduce(query, fn {key, value}, acc_query ->
+      SearchQuery.filter_by(acc_query, key, value)
+    end)
     |> Query.list_preload(:series)
     |> Repo.all()
   end
