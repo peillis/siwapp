44a3d9b971bc993d6f6eeaccdff32cfc22f81890
nurietzche <nuria.gallego@doofinder.com>
2022-02-14 09:38:51 +0100

    set_currency(amount,nil) return "#{round(amount)}"

--- lib/siwapp_web/views/page_view.ex
+++ lib/siwapp_web/views/page_view.ex
@@ -1,7 +1,13 @@
 defmodule SiwappWeb.PageView do
   use SiwappWeb, :view
 
+  @doc """
+  Returns a string of money, amount and currency, if last one
+  is provided. Otherwise, returns only string of amount.
+  """
   @spec set_currency(float | integer, atom | binary) :: binary
+  def set_currency(value, nil), do: "#{round(value)}"
+
   def set_currency(value, currency) do
     value
     |> round()
