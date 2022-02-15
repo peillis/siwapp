cd4c12c854e3ed63be74792f635a52ded9b30c58
nurietzche <nuria.gallego@doofinder.com>
2022-02-15 11:47:11 +0100

    Acabado

---
+++ lib/siwapp/customers/customer_query.ex
@@ -0,0 +1,30 @@
+defmodule Siwapp.Customers.CustomerQuery do
+  @moduledoc """
+  Module to manage Customer queries
+  """
+  alias Siwapp.Customers.Customer
+
+  import Ecto.Query
+
+  def list(limit, offset) do
+    Customer
+    |> order_by(desc: :id)
+    |> limit(^limit)
+    |> offset(^offset)
+  end
+
+  def list_for_index(limit, offset) do
+    list(limit, offset)
+    |> join(:left, [c], i in assoc(c, :invoices))
+    |> where([c, i], not (i.draft or i.failed))
+    |> group_by([c, i], c.id)
+    |> select([c, i], %Customer{
+      total: sum(i.gross_amount),
+      paid: sum(i.paid_amount),
+      currency: fragment("array_agg(?)", i.currency),
+      name: c.name,
+      identification: c.identification,
+      id: c.id
+    })
+  end
+end
