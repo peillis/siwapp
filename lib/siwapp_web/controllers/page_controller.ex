ea736c93292b482778c6052c32fb8056e5f15dbd
nurietzche <nuria.gallego@doofinder.com>
2022-02-16 11:12:57 +0100

    Preparada la función de send_email en controller

--- lib/siwapp_web/controllers/page_controller.ex
+++ lib/siwapp_web/controllers/page_controller.ex
@@ -1,7 +1,8 @@
 defmodule SiwappWeb.PageController do
   use SiwappWeb, :controller
-  alias Siwapp.Invoices
-  alias Siwapp.Templates
+
+  alias Siwapp.{Invoices, Templates}
+  alias Siwapp.Mailer
 
   @spec show_invoice(Plug.Conn.t(), map) :: Plug.Conn.t()
   def show_invoice(conn, %{"id" => id}) do
@@ -19,5 +20,23 @@ defmodule SiwappWeb.PageController do
     pdf_content = Base.decode64!(data)
 
     send_download(conn, {:binary, pdf_content}, filename: pdf_name)
+  end
+
+  def send_email(conn, %{"id" => id}) do
+    invoice = Invoices.get!(String.to_integer(id), preload: [{:items, :taxes}, :series])
+
+    case Mailer.send_email(invoice) do
+      :ok ->
+        conn
+        |> put_flash(:info, "Email successfully sent")
+
+      :error ->
+        conn
+        |> put_flash(
+          :error,
+          "Sending email was impossible. Check invoice data to see if email was provided"
+        )
+    end
+    |> redirect(to: "/")
   end
 end
