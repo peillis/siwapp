dceee04bda4f27d099906b46b8909e218207a619
Sergio Olivares <sergio.olivares@doofinder.com>
2022-02-22 10:39:27 +0100

    schema

---
+++ lib/siwapp/invoices/payment.ex
@@ -0,0 +1,43 @@
+defmodule Siwapp.Invoices.Payment do
+  @moduledoc """
+  Payment
+  """
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Siwapp.Invoices.Invoice
+
+  @fields [
+    :date,
+    :amount,
+    :notes,
+    :deleted_at,
+    :invoice_id
+  ]
+
+  @type t :: %__MODULE__{
+          id: pos_integer() | nil,
+          amount: pos_integer(),
+          notes: binary() | nil,
+          updated_at: DateTime.t() | nil,
+          inserted_at: DateTime.t() | nil,
+          deleted_at: DateTime.t() | nil,
+          invoice_id: pos_integer() | nil
+        }
+
+  schema "items" do
+    field :date, :date
+    field :amount, :integer
+    field :notes, :string
+    field :deleted_at, :utc_datetime
+    belongs_to :invoice, Invoice
+
+    timestamps()
+  end
+
+  @spec changeset(t(), map) :: Ecto.Changeset.t()
+  def changeset(payment, attrs \\ %{}) do
+    payment
+    |> cast(attrs, @fields)
+    |> foreign_key_constraint(:invoice_id)
+  end
+end
