d77c80d72e5ee4021e3d97c04463288844cb8ca4
nurietzche <nuria.gallego@doofinder.com>
2022-02-23 12:06:26 +0100

    Innecesario String.to_integer

--- lib/siwapp_web/controllers/iframe_controller.ex
+++ lib/siwapp_web/controllers/iframe_controller.ex
@@ -8,7 +8,7 @@ defmodule SiwappWeb.IframeController do
 
   @spec iframe(Plug.Conn.t(), map) :: Plug.Conn.t()
   def iframe(conn, %{"id" => id}) do
-    invoice = Invoices.get!(String.to_integer(id), preload: [{:items, :taxes}, :series])
+    invoice = Invoices.get!(id, preload: [{:items, :taxes}, :series])
     str_template = Templates.print_str_template(invoice)
 
     html(conn, str_template)
