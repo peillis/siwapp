6d1385a09159084d18e6b2c5331eaffab2a98331
Pabloj20 <pablo.rubio@doofinder.com>
2022-03-08 12:58:27 +0100

    Use sentry library

--- lib/siwapp_web/endpoint.ex
+++ lib/siwapp_web/endpoint.ex
@@ -1,4 +1,5 @@
 defmodule SiwappWeb.Endpoint do
+  use Sentry.PlugCapture
   use Phoenix.Endpoint, otp_app: :siwapp
 
   # The session will be stored in the cookie and signed,
@@ -43,6 +44,7 @@ defmodule SiwappWeb.Endpoint do
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
 
+  plug Sentry.PlugContext
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
