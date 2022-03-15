db7f85582924587e2d99eb6d2da8e2d266c1ea44
nurietzche <nuria.gallego@doofinder.com>
2022-03-15 12:27:33 +0100

    Migraciones preparadas Para migrar siwapp_demo se usa init.sql y para migrar siwapp_ror (base de datos de Doofinder) se usa real.sql
    El hash_id de customers de siwapp_dev tiene 32 caracteres y el name_slug de customers de siwapp_ror tiene 100. Lo he modificado a 100 en el nuevo siwapp.
    
    Lo mismo con currency: en siwapp_ror hay alguna null y en siwapp_dev invoices y recurring_invoices tiene not-null constraint. Estoy poniendo las currency que sean NULL a USD.
    
    En siwapp_ror hay una clave de identificación de cliente repetida. Cambia name y name_slug pero entra en conflicto con unique constraint customers_identification_index. He cambiado ese index a que la unicidad sea en la combinación de name e identification.
    
    PD: Todavía no ha terminado de correr el fichero real.sql en mi PC porque hay muchos datos, pero lo voy subiendo.

--- priv/repo/migrations/20220207124821_create_customers.exs
+++ priv/repo/migrations/20220207124821_create_customers.exs
@@ -5,7 +5,7 @@ defmodule Siwapp.Repo.Migrations.CreateCustomers do
     create table(:customers) do
       add :name, :string, size: 100
       add :identification, :string, size: 50
-      add :hash_id, :string, size: 32
+      add :hash_id, :string, size: 100
       add :email, :string, size: 100
       add :contact_person, :string, size: 100
       add :invoicing_address, :text
@@ -15,7 +15,7 @@ defmodule Siwapp.Repo.Migrations.CreateCustomers do
       timestamps()
     end
 
-    create index(:customers, [:identification], unique: true)
+    create index(:customers, [:identification, :name], unique: true)
     create index(:customers, [:hash_id], unique: true)
   end
 end
--- priv/repo/real.sql
+++ priv/repo/real.sql
@@ -2,21 +2,21 @@
 DROP DATABASE IF EXISTS prueba_siwapp;
 SELECT pg_terminate_backend(pg_stat_activity.pid)
 FROM pg_stat_activity
-WHERE pg_stat_activity.datname = 'siwapp_demo'
+WHERE pg_stat_activity.datname = 'siwapp_ror'
 AND pid <> pg_backend_pid();
 CREATE DATABASE prueba_siwapp
-  WITH TEMPLATE siwapp_demo
+  WITH TEMPLATE siwapp_ror
     OWNER postgres;
 
 \c prueba_siwapp
-ALTER TABLE taxes RENAME TO taxes_demo;
-ALTER TABLE series RENAME TO series_demo;
-ALTER TABLE items RENAME TO items_demo;
-ALTER TABLE items_demo DROP COLUMN product_id;
-ALTER TABLE items_demo DROP COLUMN deleted_at;
-ALTER TABLE customers RENAME TO customers_demo;
-ALTER TABLE payments RENAME TO payments_demo;
-ALTER TABLE items_taxes RENAME TO items_taxes_demo;
+ALTER TABLE taxes RENAME TO taxes_ror;
+ALTER TABLE series RENAME TO series_ror;
+ALTER TABLE items RENAME TO items_ror;
+ALTER TABLE items_ror DROP COLUMN product_id;
+ALTER TABLE items_ror DROP COLUMN deleted_at;
+ALTER TABLE customers RENAME TO customers_ror;
+ALTER TABLE payments RENAME TO payments_ror;
+ALTER TABLE items_taxes RENAME TO items_taxes_ror;
 
 CREATE FUNCTION change_column_with_function(table_name text, column_name text, t text, fun text)
 RETURNS void
@@ -61,14 +61,14 @@ BEGIN
     return result;
 END; $$;
 
-SELECT change_column_with_function('commons', 'net_amount', 'integer', 'to_cents');
-SELECT change_column_with_function('commons', 'gross_amount', 'integer', 'to_cents');
-SELECT change_column_with_function('commons', 'paid_amount', 'integer', 'to_cents');
-SELECT change_column_with_function('commons', 'currency', 'character varying(3)', 'currency_conversion');
-SELECT change_column_with_function('items_demo', 'unitary_cost', 'integer', 'to_cents');
-SELECT change_column_with_function('payments_demo', 'amount', 'integer', 'to_cents');
-SELECT change_column_with_function('commons', 'period_type', 'character varying(8)', 'period_type_conversion');
-ALTER TABLE customers_demo ALTER COLUMN meta_attributes TYPE jsonb USING meta_attributes::jsonb;
+SELECT change_column_with_function('commons', 'net_amount', 'integer', 'to_cents') AS net_amount_to_cents;
+SELECT change_column_with_function('commons', 'gross_amount', 'integer', 'to_cents') AS gross_amount_to_cents;
+SELECT change_column_with_function('commons', 'paid_amount', 'integer', 'to_cents') AS paid_amount_to_cents;
+SELECT change_column_with_function('commons', 'currency', 'character varying(3)', 'currency_conversion') AS currency_conversion;
+SELECT change_column_with_function('items_ror', 'unitary_cost', 'integer', 'to_cents') AS unitary_cost_to_cents;
+SELECT change_column_with_function('payments_ror', 'amount', 'integer', 'to_cents') AS amount_to_cents;
+SELECT change_column_with_function('commons', 'period_type', 'character varying(8)', 'period_type_conversion') AS period_type_conversion;
+ALTER TABLE customers_ror ALTER COLUMN meta_attributes TYPE jsonb USING meta_attributes::jsonb;
 ALTER TABLE commons ALTER COLUMN meta_attributes TYPE jsonb USING meta_attributes::jsonb;
 
 DROP TABLE schema_migrations;
@@ -78,12 +78,10 @@ DROP TABLE users;
 DROP TABLE webhook_logs;
 DROP TABLE tags;
 DROP TABLE taggings;
-DROP TABLE ar_internal_metadata;
 DROP TABLE products;
 DROP FUNCTION change_column_with_function;
 DROP FUNCTION to_cents;
 DROP FUNCTION period_type_conversion;
-DROP FUNCTION text_to_jsonb;
 
 \c siwapp_dev
 CREATE EXTENSION postgres_fdw;
@@ -104,20 +102,22 @@ BEGIN
 END; $$;
 
 --taxes
--- value in demo was numeric, ours will round decimals
-ALTER TABLE taxes_demo DROP COLUMN deleted_at;
-INSERT INTO taxes SELECT * FROM taxes_demo;
+-- value in ror was numeric, ours will round decimals
+ALTER TABLE taxes_ror DROP COLUMN deleted_at;
+INSERT INTO taxes SELECT * FROM taxes_ror;
+SELECT 'taxes done' AS msg;
 
 --series
-ALTER TABLE series_demo DROP COLUMN deleted_at;
-INSERT INTO series SELECT * FROM series_demo;
+ALTER TABLE series_ror DROP COLUMN deleted_at;
+INSERT INTO series SELECT * FROM series_ror;
+SELECT 'series done'AS msg;
 
--- I'm not inserting meta_attributes because they are all null in siwapp_demo and type text, so I don't know yet how to convert them to jsonb
 --customers
-ALTER TABLE customers_demo DROP COLUMN deleted_at;
-ALTER TABLE customers_demo DROP COLUMN active;
+ALTER TABLE customers_ror DROP COLUMN deleted_at;
+ALTER TABLE customers_ror DROP COLUMN active;
 INSERT INTO customers(id, name, hash_id, identification, email, contact_person, invoicing_address, shipping_address, meta_attributes, inserted_at, updated_at)
-  SELECT *, NOW(), NOW() FROM customers_demo;
+  SELECT *, NOW(), NOW() FROM customers_ror;
+SELECT 'customers done' AS msg;
 
 --recurring_invoices
 CREATE FUNCTION new_recurring_invoice_id(old_recurring_invoice_id integer)
@@ -138,12 +138,11 @@ DECLARE result jsonb;
 BEGIN
     DROP TABLE IF EXISTS items_one_recurring_invoice;
     CREATE TABLE items_one_recurring_invoice(index serial, item jsonb);
-    DROP SEQUENCE IF EXISTS index;
     CREATE SEQUENCE index START 1 OWNED BY items_one_recurring_invoice.index;
     INSERT INTO items_one_recurring_invoice(item)
       SELECT row_to_json(t) FROM
       (SELECT taxes, discount, quantity, description, unitary_cost
-        FROM items_demo_extended WHERE common_id=id) t;
+        FROM items_ror_extended WHERE common_id=id) t;
     WITH data(index, item) AS (SELECT * FROM items_one_recurring_invoice)
       SELECT json_object_agg(index-1,item) INTO result FROM data;
     return result;
@@ -151,14 +150,14 @@ END; $$;
 
 CREATE TABLE items_taxes_names(item_id integer, taxes text[]);
 INSERT INTO items_taxes_names
-  SELECT items_demo.id, array_remove(array_agg(taxes.name), NULL) FROM items_demo
-    LEFT JOIN items_taxes_demo ON items_demo.id=item_id
-      LEFT JOIN taxes ON taxes.id=tax_id GROUP BY item_id, items_demo.id ORDER BY items_demo.id ASC;
+  SELECT items_ror.id, array_remove(array_agg(taxes.name), NULL) FROM items_ror
+    LEFT JOIN items_taxes_ror ON items_ror.id=item_id
+      LEFT JOIN taxes ON taxes.id=tax_id GROUP BY item_id, items_ror.id ORDER BY items_ror.id ASC;
 
-CREATE TABLE items_demo_extended(common_id bigint, taxes text[], discount integer, quantity integer, description character varying(20000), unitary_cost integer);
-INSERT INTO items_demo_extended
-  SELECT common_id, taxes, discount, quantity, description, unitary_cost FROM items_demo
-    LEFT JOIN items_taxes_names ON items_taxes_names.item_id=items_demo.id WHERE not is_invoice(common_id);
+CREATE TABLE items_ror_extended(common_id bigint, taxes text[], discount integer, quantity integer, description character varying(20000), unitary_cost integer);
+INSERT INTO items_ror_extended
+  SELECT common_id, taxes, discount, quantity, description, unitary_cost FROM items_ror
+    LEFT JOIN items_taxes_names ON items_taxes_names.item_id=items_ror.id WHERE not is_invoice(common_id);
 
 INSERT INTO recurring_invoices(series_id, customer_id, name, identification, email, invoicing_address, shipping_address, contact_person, terms, notes, net_amount, gross_amount, send_by_email, days_to_due, enabled, max_ocurrences, period, period_type, starting_date, finishing_date, inserted_at, updated_at, currency, items, meta_attributes)
   SELECT series_id, customer_id, name, identification, email, invoicing_address, shipping_address, contact_person, terms, notes, net_amount, gross_amount, sent_by_email, days_to_due, enabled, max_occurrences, period, period_type, starting_date, finishing_date, created_at, updated_at, currency, build_items(id), meta_attributes
@@ -168,6 +167,7 @@ INSERT INTO recurring_invoices_id_conversion
   SELECT recurring_invoices.id, commons.id FROM recurring_invoices
     INNER JOIN commons ON recurring_invoices.name = commons.name AND recurring_invoices.identification = commons.identification
       ORDER BY recurring_invoices.id ASC;
+SELECT 'recurring_invoices done' AS msg;
 
 --invoices
 CREATE FUNCTION new_invoice_id(old_invoice_id integer)
@@ -188,16 +188,19 @@ INSERT INTO invoices_id_conversion
   SELECT invoices.id, commons.id FROM invoices
     INNER JOIN commons ON invoices.name = commons.name AND invoices.identification = commons.identification
       ORDER BY invoices.id ASC;
+SELECT 'invoices done' AS msg;
 
 --items
--- discount and quantity in demo were numeric, ours will round decimals
+-- discount and quantity in ror were numeric, ours will round decimals
 INSERT INTO items(quantity, discount, description, unitary_cost, invoice_id)
   SELECT quantity, discount, description, unitary_cost, new_invoice_id(common_id)
-    FROM items_demo WHERE is_invoice(common_id)=true;
+    FROM items_ror WHERE is_invoice(common_id)=true;
+SELECT 'items done' AS msg;
 
 -- payments
 INSERT INTO payments("date", amount, notes, invoice_id, inserted_at, updated_at)
-  SELECT "date", amount, notes, new_invoice_id(invoice_id), created_at, updated_at FROM payments_demo;
+  SELECT "date", amount, notes, new_invoice_id(invoice_id), created_at, updated_at FROM payments_ror;
+SELECT 'payments done' AS msg;
 
 -- items_taxes
 CREATE FUNCTION new_item_id(old_item_id integer)
@@ -206,29 +209,30 @@ LANGUAGE plpgsql
 AS $$
 DECLARE result integer;
 BEGIN
-    SELECT item_id INTO result FROM items_id_conversion WHERE item_demo_id=old_item_id;
+    SELECT item_id INTO result FROM items_id_conversion WHERE item_ror_id=old_item_id;
     return result;
 END; $$;
-CREATE TABLE items_id_conversion(item_id integer, item_demo_id integer);
+CREATE TABLE items_id_conversion(item_id integer, item_ror_id integer);
 INSERT INTO items_id_conversion
-  SELECT items.id, items_demo.id FROM items
-    INNER JOIN items_demo ON items.description = items_demo.description
+  SELECT items.id, items_ror.id FROM items
+    INNER JOIN items_ror ON items.description = items_ror.description
       ORDER BY items.id ASC;
-INSERT INTO items_taxes SELECT new_item_id(item_id), tax_id FROM items_taxes_demo;
+INSERT INTO items_taxes SELECT new_item_id(item_id), tax_id FROM items_taxes_ror;
+SELECT 'items_taxes done' AS msg;
 
-DROP FOREIGN TABLE taxes_demo;
-DROP FOREIGN TABLE series_demo;
-DROP FOREIGN TABLE customers_demo;
+DROP FOREIGN TABLE taxes_ror;
+DROP FOREIGN TABLE series_ror;
+DROP FOREIGN TABLE customers_ror;
 DROP FOREIGN TABLE commons;
-DROP FOREIGN TABLE items_taxes_demo;
-DROP FOREIGN TABLE items_demo;
-DROP FOREIGN TABLE payments_demo;
+DROP FOREIGN TABLE items_taxes_ror;
+DROP FOREIGN TABLE items_ror;
+DROP FOREIGN TABLE payments_ror;
 DROP TABLE recurring_invoices_id_conversion;
 DROP TABLE invoices_id_conversion;
 DROP TABLE items_id_conversion;
 DROP TABLE items_one_recurring_invoice;
 DROP TABLE items_taxes_names;
-DROP TABLE items_demo_extended;
+DROP TABLE items_ror_extended;
 DROP FUNCTION is_invoice;
 DROP FUNCTION build_items;
 DROP FUNCTION new_recurring_invoice_id;
