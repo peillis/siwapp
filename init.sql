edcf980508610d486fbea2617a835f7d028c6d91
nurietzche <nuria.gallego@doofinder.com>
2022-03-15 08:40:15 +0100

    Readability

--- init.sql
+++ init.sql
@@ -124,25 +124,25 @@ BEGIN
       SELECT row_to_json(t) FROM
       (SELECT taxes, discount, quantity, description, unitary_cost
         FROM items_demo_extended WHERE common_id=id) t;
-    WITH data(index, item) AS (SELECT * FROM items_one_recurring_invoice) SELECT json_object_agg(index-1,item) INTO result FROM data;
+    WITH data(index, item) AS (SELECT * FROM items_one_recurring_invoice)
+      SELECT json_object_agg(index-1,item) INTO result FROM data;
     return result;
 END; $$;
 
 CREATE TABLE items_taxes_names(item_id integer, taxes text[]);
 INSERT INTO items_taxes_names
-SELECT items_demo.id, array_remove(array_agg(taxes.name), NULL) FROM items_demo
-LEFT JOIN items_taxes_demo ON items_demo.id=item_id
-LEFT JOIN taxes ON taxes.id=tax_id GROUP BY item_id, items_demo.id ORDER BY items_demo.id ASC;
+  SELECT items_demo.id, array_remove(array_agg(taxes.name), NULL) FROM items_demo
+    LEFT JOIN items_taxes_demo ON items_demo.id=item_id
+      LEFT JOIN taxes ON taxes.id=tax_id GROUP BY item_id, items_demo.id ORDER BY items_demo.id ASC;
 
 CREATE TABLE items_demo_extended(common_id bigint, taxes text[], discount integer, quantity integer, description character varying(20000), unitary_cost integer);
 INSERT INTO items_demo_extended
   SELECT common_id, taxes, discount, quantity, description, unitary_cost FROM items_demo
-  LEFT JOIN items_taxes_names ON items_taxes_names.item_id=items_demo.id WHERE not is_invoice(common_id);
-SELECT json_agg(t) FROM (SELECT taxes, discount, quantity, description, unitary_cost FROM items_demo_extended WHERE common_id=1) t;
+    LEFT JOIN items_taxes_names ON items_taxes_names.item_id=items_demo.id WHERE not is_invoice(common_id);
 
 INSERT INTO recurring_invoices(series_id, customer_id, name, identification, email, invoicing_address, shipping_address, contact_person, terms, notes, net_amount, gross_amount, send_by_email, days_to_due, enabled, max_ocurrences, period, period_type, starting_date, finishing_date, inserted_at, updated_at, currency, items)
-SELECT series_id, customer_id, name, identification, email, invoicing_address, shipping_address, contact_person, terms, notes, net_amount, gross_amount, sent_by_email, days_to_due, enabled, max_occurrences, period, period_type, starting_date, finishing_date, created_at, updated_at, currency, build_items(id)
-FROM commons WHERE type='RecurringInvoice' ORDER BY id ASC;
+  SELECT series_id, customer_id, name, identification, email, invoicing_address, shipping_address, contact_person, terms, notes, net_amount, gross_amount, sent_by_email, days_to_due, enabled, max_occurrences, period, period_type, starting_date, finishing_date, created_at, updated_at, currency, build_items(id)
+    FROM commons WHERE type='RecurringInvoice' ORDER BY id ASC;
 
 --invoices
 CREATE FUNCTION new_invoice_id(old_invoice_id integer)
@@ -156,16 +156,23 @@ BEGIN
 END; $$;
 
 INSERT INTO invoices(series_id, customer_id, name, identification, email, invoicing_address, shipping_address, contact_person, terms, notes, net_amount, gross_amount, paid_amount, draft, paid, sent_by_email, "number", recurring_invoice_id, issue_date, due_date, inserted_at, updated_at, deleted_at, failed, currency)
-SELECT series_id, customer_id, name, identification, email, invoicing_address, shipping_address, contact_person, terms, notes, net_amount, gross_amount, paid_amount, draft, paid, sent_by_email, "number", recurring_invoice_id, issue_date, due_date, created_at, updated_at, deleted_at, failed, currency
-FROM commons WHERE type='Invoice' ORDER BY id ASC;
+  SELECT series_id, customer_id, name, identification, email, invoicing_address, shipping_address, contact_person, terms, notes, net_amount, gross_amount, paid_amount, draft, paid, sent_by_email, "number", recurring_invoice_id, issue_date, due_date, created_at, updated_at, deleted_at, failed, currency
+    FROM commons WHERE type='Invoice' ORDER BY id ASC;
 CREATE TABLE invoices_id_conversion(invoice_id integer, common_id integer);
-INSERT INTO invoices_id_conversion SELECT invoices.id, commons.id FROM invoices INNER JOIN commons ON invoices.name = commons.name AND invoices.identification = commons.identification ORDER BY invoices.id;
+INSERT INTO invoices_id_conversion
+  SELECT invoices.id, commons.id FROM invoices
+    INNER JOIN commons ON invoices.name = commons.name AND invoices.identification = commons.identification
+      ORDER BY invoices.id;
 
 --items
-INSERT INTO items(quantity, discount, description, unitary_cost, invoice_id) SELECT quantity, discount, description, unitary_cost, new_invoice_id(common_id) FROM items_demo WHERE is_invoice(common_id)=true;
+-- discount and quantity in demo were numeric, ours will round decimals
+INSERT INTO items(quantity, discount, description, unitary_cost, invoice_id)
+  SELECT quantity, discount, description, unitary_cost, new_invoice_id(common_id)
+    FROM items_demo WHERE is_invoice(common_id)=true;
 
 -- payments
-INSERT INTO payments("date", amount, notes, invoice_id, inserted_at, updated_at) SELECT "date", amount, notes, new_invoice_id(invoice_id), created_at, updated_at FROM payments_demo;
+INSERT INTO payments("date", amount, notes, invoice_id, inserted_at, updated_at)
+  SELECT "date", amount, notes, new_invoice_id(invoice_id), created_at, updated_at FROM payments_demo;
 
 -- items_taxes
 CREATE FUNCTION new_item_id(old_item_id integer)
@@ -178,7 +185,10 @@ BEGIN
     return result;
 END; $$;
 CREATE TABLE items_id_conversion(item_id integer, item_demo_id integer);
-INSERT INTO items_id_conversion SELECT items.id, items_demo.id FROM items INNER JOIN items_demo ON items.description = items_demo.description ORDER BY items.id;
+INSERT INTO items_id_conversion
+  SELECT items.id, items_demo.id FROM items
+    INNER JOIN items_demo ON items.description = items_demo.description
+      ORDER BY items.id ASC;
 INSERT INTO items_taxes SELECT new_item_id(item_id), tax_id FROM items_taxes_demo;
 
 DROP FOREIGN TABLE taxes_demo;
