2bc7e4959376d3a1a98b75211e5f0b3e8a1e4e5f
Pabloj20 <pablo.rubio@doofinder.com>
2022-03-18 12:24:11 +0100

    graphql tests

--- test/siwapp_web/schema_test.exs
+++ test/siwapp_web/schema_test.exs
@@ -2,28 +2,136 @@ defmodule SiwappWeb.SchemaTest do
   use SiwappWeb.ConnCase, async: true
 
   import Siwapp.InvoicesFixtures
-  import Siwapp.CustomersFixtures
+  import Siwapp.CommonsFixtures
+  import Siwapp.SettingsFixtures
 
   setup do
-    invoice_fixture(%{name: "test1"})
-    invoice_fixture(%{name: "test2"})
+    series = series_fixture(%{name: "A-Series", code: "A-"})
+    taxes_fixture(%{name: "VAT", value: 21, default: true})
+    taxes_fixture(%{name: "RETENTION", value: -15})
+    settings_fixture()
+
+    invoices =
+      Enum.map(1..11, fn _i ->
+        invoice_fixture(%{name: "test_name", identification: "test_id"})
+      end)
+
+    %{invoices: invoices, series: series}
   end
 
   describe "invoices" do
-    test "list invoices", %{conn: conn} do
+    test "list invoices by a customer_id and get its name", %{conn: conn, invoices: invoices} do
+      invoice = hd(invoices)
+
       query = """
         query {
-          invoices {
+          invoices(customer_id: #{invoice.customer_id} ) {
+            name
+            customer_id
+          }
+        }
+      """
+
+      data_result =
+        Enum.map(1..10, fn _i ->
+          %{"name" => invoice.name, "customer_id" => "#{invoice.customer_id}"}
+        end)
+
+      res =
+        conn
+        |> post("/graphql/graphiql", %{query: query})
+        |> json_response(200)
+
+      assert res == %{"data" => %{"invoices" => data_result}}
+    end
+
+    test "Create an invoice", %{conn: conn, series: series} do
+      query = """
+        mutation {
+          create_invoice(name: "test_name", series_code: "#{series.code}") {
+            name
+            series_id
+          }
+        }
+      """
+
+      res =
+        conn
+        |> post("/graphql/graphiql", %{query: query})
+        |> json_response(200)
+
+      assert res == %{
+               "data" => %{
+                 "create_invoice" => %{"name" => "test_name", "series_id" => "#{series.id}"}
+               }
+             }
+    end
+
+    test "Update an invoice", %{conn: conn, invoices: invoices} do
+      invoice = hd(invoices)
+
+      query = """
+        mutation {
+          update_invoice(id: #{invoice.id}, email: "info@example.com") {
+            name
+            email
+          }
+        }
+      """
+
+      res =
+        conn
+        |> post("/graphql/graphiql", %{query: query})
+        |> json_response(200)
+
+      assert res == %{
+               "data" => %{
+                 "update_invoice" => %{"name" => invoice.name, "email" => "info@example.com"}
+               }
+             }
+    end
+
+    test "Delete an invoice", %{conn: conn, invoices: invoices} do
+      invoice = hd(invoices)
+
+      query = """
+        mutation {
+          delete_invoice(id: #{invoice.id}) {
             name
           }
         }
       """
 
-      conn
-      |> post("/graphql/graphiql", %{query: query})
-      |> json_response(200)
+      res =
+        conn
+        |> post("/graphql/graphiql", %{query: query})
+        |> json_response(200)
 
-      assert %{"data" => %{"invoices" => [%{"name" => "test1"}, %{"name" => "test2"}]}}
+      assert res == %{"data" => %{"delete_invoice" => %{"name" => invoice.name}}}
+    end
+  end
+
+  describe "customers" do
+    test "list customers using default options", %{conn: conn} do
+      query = """
+        query {
+          customers {
+            name
+            identification
+          }
+        }
+      """
+
+      res =
+        conn
+        |> post("/graphql/graphiql", %{query: query})
+        |> json_response(200)
+
+      assert res == %{
+               "data" => %{
+                 "customers" => [%{"name" => "test_name", "identification" => "test_id"}]
+               }
+             }
     end
   end
 end
