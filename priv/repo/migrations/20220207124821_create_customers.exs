f75b3612a130274fd2380e4cb6b2d5fda74105a4
nurietzsche2021 <92876768+nurietzsche2021@users.noreply.github.com>
2022-03-18 16:25:14 +0100

    Cambios necesarios: (#510)
    - La db de Doofinder usa un hash_id de longitud 100. Lo he cambiado para
      que coincida la migración con las tablas usadas.
    - Next_number_in_series no estaba cribando las invoices que tienen
      number NULL y al sumar uno a este, petaba

--- priv/repo/migrations/20220207124821_create_customers.exs
+++ priv/repo/migrations/20220207124821_create_customers.exs
@@ -5,7 +5,7 @@ defmodule Siwapp.Repo.Migrations.CreateCustomers do
     create table(:customers) do
       add :name, :string, size: 100
       add :identification, :string, size: 50
-      add :hash_id, :string, size: 32
+      add :hash_id, :string, size: 100
       add :email, :string, size: 100
       add :contact_person, :string, size: 100
       add :invoicing_address, :text
