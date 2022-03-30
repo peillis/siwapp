e78450b42b96cb8a472c5a6f5ad30db506e602bd
peillis <peillis@users.noreply.github.com>
2022-03-30 17:58:22 +0200

    Enrique/changes (#529)
    * design change
    
    * mix format

--- lib/siwapp_web/live/recurring_invoices_live/index.ex
+++ lib/siwapp_web/live/recurring_invoices_live/index.ex
@@ -4,7 +4,7 @@ defmodule SiwappWeb.RecurringInvoicesLive.Index do
   """
   use SiwappWeb, :live_view
 
-  import SiwappWeb.PageView, only: [money_format: 3, money_format: 2]
+  import SiwappWeb.PageView, only: [money_format: 2]
 
   alias Siwapp.Invoices.Statistics
   alias Siwapp.RecurringInvoices
