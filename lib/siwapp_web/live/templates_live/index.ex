6fa7dee826cd89d92b29b4ac4818e64a21ee4164
nurietzsche <nuria.gallego@doofinder.com>
2022-04-05 18:57:16 +0200

    Impedimento para dejar defaults vacíos

--- lib/siwapp_web/live/templates_live/index.ex
+++ lib/siwapp_web/live/templates_live/index.ex
@@ -17,9 +17,30 @@ defmodule SiwappWeb.TemplatesLive.Index do
   @impl Phoenix.LiveView
   def handle_event("defaultClicked", %{"id" => id, "type" => type}, socket) do
     template = id |> String.to_integer() |> Templates.get()
-    Templates.set_default(String.to_atom(type), template)
 
-    {:noreply, assign(socket, templates: Templates.list())}
+    socket = assign(socket, templates: Templates.list())
+
+    cond do
+      template.print_default and type == "print" ->
+        {:noreply,
+         put_flash(
+           socket,
+           :error,
+           "You must have one default template. Please select one to swipe defaults."
+         )}
+
+      template.email_default and type == "email" ->
+        {:noreply,
+         put_flash(
+           socket,
+           :error,
+           "You must have one default template. Please select one to swipe defaults."
+         )}
+
+      true ->
+        Templates.set_default(String.to_atom(type), template)
+        {:noreply, push_redirect(socket, to: Routes.templates_index_path(socket, :index))}
+    end
   end
 
   def handle_event("edit", %{"id" => id}, socket) do
