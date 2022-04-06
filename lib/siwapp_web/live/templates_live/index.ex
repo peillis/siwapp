6584e1e532b7c238376c4b233d9e77e19a468b4c
nurietzsche <nuria.gallego@doofinder.com>
2022-04-06 10:48:16 +0200

    Using clear flash

--- lib/siwapp_web/live/templates_live/index.ex
+++ lib/siwapp_web/live/templates_live/index.ex
@@ -18,29 +18,31 @@ defmodule SiwappWeb.TemplatesLive.Index do
   def handle_event("defaultClicked", %{"id" => id, "type" => type}, socket) do
     template = id |> String.to_integer() |> Templates.get()
 
-    socket = assign(socket, templates: Templates.list())
+    socket =
+      cond do
+        template.print_default and type == "print" ->
+          put_flash(
+            socket,
+            :error,
+            "You must have one default template. Please select one to swipe defaults."
+          )
 
-    cond do
-      template.print_default and type == "print" ->
-        {:noreply,
-         put_flash(
-           socket,
-           :error,
-           "You must have one default template. Please select one to swipe defaults."
-         )}
+        template.email_default and type == "email" ->
+          put_flash(
+            socket,
+            :error,
+            "You must have one default template. Please select one to swipe defaults."
+          )
 
-      template.email_default and type == "email" ->
-        {:noreply,
-         put_flash(
-           socket,
-           :error,
-           "You must have one default template. Please select one to swipe defaults."
-         )}
+        true ->
+          Templates.set_default(String.to_atom(type), template)
 
-      true ->
-        Templates.set_default(String.to_atom(type), template)
-        {:noreply, push_redirect(socket, to: Routes.templates_index_path(socket, :index))}
-    end
+          socket
+          |> assign(templates: Templates.list())
+          |> clear_flash()
+      end
+
+    {:noreply, socket}
   end
 
   def handle_event("edit", %{"id" => id}, socket) do
